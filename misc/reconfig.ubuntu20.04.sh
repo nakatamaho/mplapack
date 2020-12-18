fcdaffbec35f37d1789f94cd9362c619b0902cff
nakata maho <maho.nakata@gmail.com>
2020-12-18 16:20:53 +0900

    .

---
+++ misc/reconfig.ubuntu20.04.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CXX="g++-9" ; export CXX
+CC="gcc-9" ; export CC
+FC="gfortran-9"; export FC
+F77="gfortran-9"; export F77
+
+aclocal ; autoconf ; automake
+autoreconf --force --install
+
+#CXXFLAGS="-O2 -Wall -Werror -pedantic-errors" ; export CXXFLAGS
+#CPPFLAGS="-O2 -Wall -Werror -pedantic-errors" ; export CPPFLAGS  
+#CFLAGS="-O2 -Wall -Werror -pedantic-errors" ; export CFLAGS  
+
+./configure --prefix=$HOME/MPACK
+
