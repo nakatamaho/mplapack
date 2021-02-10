700fb898130d51168446efc89ef0c618c61b8482
Nakata Maho <maho.nakata@gmail.com>
2021-02-10 19:01:14 +0900

    .

---
+++ misc/reconfig.ubuntu20.04.arm64.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CXX="g++-9" ; export CXX
+CC="gcc-9" ; export CC
+FC="gfortran-9"; export FC
+F77="gfortran-9"; export F77
+
+aclocal ; autoconf ; automake --add-missing
+autoreconf --force --install
+
+#CXXFLAGS="-O2 -Wall -Werror -pedantic-errors" ; export CXXFLAGS
+#CPPFLAGS="-O2 -Wall -Werror -pedantic-errors" ; export CPPFLAGS  
+#CFLAGS="-O2 -Wall -Werror -pedantic-errors" ; export CFLAGS  
+
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-__float128=no
+
+
+
