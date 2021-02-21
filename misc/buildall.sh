51b8055c68fbadb572577ca95a254cd22e4892ac
NAKATA Maho <maho.nakata@gmail.com>
2021-02-21 20:35:42 +0900

    .

--- misc/buildall.sh
+++ misc/buildall.sh
@@ -9,7 +9,7 @@ cd gcc
 md5sum ~/mpack-$MPACKVER.tar.gz
 tar xvfz ~/mpack-$MPACKVER.tar.gz
 cd mpack-$MPACKVER
-./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-__float128=yes --prefix=/home/maho/MPACK.GCC
+./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-__float128=yes --prefix=/home/docker/MPACK.GCC
 /usr/bin/time make -j20
 /usr/bin/time make install
 cd ..
@@ -24,7 +24,7 @@ source /opt/intel/composer_xe_2013/bin/compilervars.sh intel64
 export CC="icc"
 export CXX="icpc"
 export FC="ifort"
-./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-__float128=yes --prefix=/home/maho/MPACK.ICC --with-external-blas="-mkl" --with-external-lapack="-mkl"
+./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-__float128=yes --prefix=/home/docker/MPACK.ICC --with-external-blas="-mkl" --with-external-lapack="-mkl"
 /usr/bin/time make -j20
 /usr/bin/time make install
 cd ..
--- misc/cleanbuild.sh
+++ misc/cleanbuild.sh
@@ -7,6 +7,6 @@ automake --add-missing
 autoreconf
 autoconf
 automake
-./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-__float128=yes --prefix=/home/maho/MPACK.GCC
+./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-__float128=yes --prefix=/home/docker/MPACK.GCC
 /usr/bin/time make -j20
 /usr/bin/time make install
