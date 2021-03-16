cf2f4fc112057325981999a76c76b04947fbbb4e
NAKATA Maho <maho.nakata@gmail.com>
2021-03-16 14:40:49 +0900

    .

---
+++ misc/reconfig.ubuntu20.04.intel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CXX="icpc" ; export CXX
+CC="icc" ; export CC
+FC="ifort"; export FC
+F77="ifort"; export F77
+
+pushd mplapack/debug ; bash gen.Makefile.am.sh ; popd
+
+aclocal ; autoconf ; automake --add-missing
+autoreconf --force --install
+
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-longdouble=yes --enable-debug=yes
+
