7a33d362aa018545a2465080c8c8a5e336c17306
Maho Nakata <maho.nakata@gmail.com>
2021-03-25 14:43:11 +0900

    .

--- misc/reconfig.ubuntu20.04.intel.sh
+++ misc/reconfig.ubuntu20.04.intel.sh
@@ -14,5 +14,5 @@ pushd mplapack/debug ; bash gen.Makefile.am.sh ; popd
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
 
-./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-debug=yes
+./configure --prefix=$HOME/MPLAPACK --with-system-gmp=/usr --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-debug=yes
 
