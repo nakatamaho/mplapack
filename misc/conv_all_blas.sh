13921affa127f4e8276532951b182bb4f85182b4
NAKATA Maho <maho.nakata@gmail.com>
2021-04-05 09:23:15 +0900

    .

--- misc/conv_all_blas.sh
+++ misc/conv_all_blas.sh
@@ -18,7 +18,7 @@ cat BLAS_LIST BLAS_LIST_  BLAS_LIST__ > ll
 mv ll BLAS_LIST
 rm BLAS_LIST_*
 
-for _file in "$FILES Mlsame.cpp  Mxerbla.cpp"; do
+for _file in "$FILES"; do
 bash ~/mplapack/misc/fem_convert_blas.sh $_file
 oldfilename=`basename $_file | sed -e 's/\.f$//'`
 newfilename=`basename $_file | sed -e 's/^zdscal/CRscal/g' -e 's/^zdrot/CRrot/g' -e 's/^dcabs/RCabs/g' -e 's/^dzasum/RCasum/g' -e 's/^dznrm2/RCnrm2/g' | sed -e 's/^d/R/' -e 's/^z/C/' -e 's/^id/iR/' -e 's/^iz/iC/' -e 's/\.f$//'`
@@ -27,5 +27,8 @@ mv ${newfilename}.cpp_  ${newfilename}.cpp
 /usr/local/bin/ctags -x --c++-kinds=pf --language-force=c++ --_xformat='%{typeref} %{name} %{signature};' ${newfilename}.cpp |  tr ':' ' ' | sed -e 's/^typename //' > ${newfilename}.hpp
 rm ${oldfilename}.cpp
 done
+/usr/local/bin/ctags -x --c++-kinds=pf --language-force=c++ --_xformat='%{typeref} %{name} %{signature};' Mxerbla.cpp |  tr ':' ' ' | sed -e 's/^typename //' > Mxerbla.hpp
+/usr/local/bin/ctags -x --c++-kinds=pf --language-force=c++ --_xformat='%{typeref} %{name} %{signature};' Mlsame.cpp |  tr ':' ' ' | sed -e 's/^typename //' > Mlsame.hpp
+
 cp BLAS_LIST ~/mplapack/misc/
 cat ~/mplapack/misc/header_blas *hpp > mpblas.h
