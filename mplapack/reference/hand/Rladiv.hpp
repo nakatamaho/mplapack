1a65c9f9650e1ad0776678ff0853930f71f0e948
NAKATA Maho <maho.nakata@gmail.com>
2021-04-06 08:39:07 +0900

    .

---
+++ mplapack/reference/hand/Rladiv.hpp
@@ -0,0 +1,3 @@
+REAL Rladiv2 (REAL const & a,REAL const & b,REAL const & c,REAL const & d,REAL const & r,REAL const & t);
+void Rladiv (REAL const & a,REAL const & b,REAL const & c,REAL const & d,REAL & p,REAL & q);
+void Rladiv1 (REAL & a,REAL const & b,REAL const & c,REAL const & d,REAL & p,REAL & q);
