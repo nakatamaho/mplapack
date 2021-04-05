ac297d2eccd4d429cc31dbe13311ae5d815e29da
Nakata Maho <maho.nakata@gmail.com>
2021-04-05 16:38:28 +0900

    .

---
+++ misc/reconfig.centos7.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+USE_CCACHE=yes
+export PATH=/opt/rh/devtoolset-9/root/usr/bin/:$PATH
+
+if [ x$USE_CCACHE = x"yes" ] ; then
+CXX="ccache g++" ; export CXX
+CC="ccache gcc" ; export CC
+FC="gfortran"; export FC
+F77="gfortran"; export F77
+ccache -M 20G
+else
+CXX="g++" ; export CXX
+CC="gcc" ; export CC
+FC="gfortran"; export FC
+F77="gfortran"; export F77
+fi
+
+pushd mplapack/debug ; bash gen.Makefile.am.sh ; popd
+
+aclocal ; autoconf ; automake --add-missing
+autoreconf --force --install
+if [ `uname -m` = "x86_64" ]; then
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-debug=yes
+else
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-debug=yes
+fi
