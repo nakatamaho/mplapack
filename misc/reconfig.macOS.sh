bad9ae968504b9c82f545bf22459ef5a9497ad8a
Maho Nakata <maho.nakata@gmail.com>
2021-04-24 06:36:19 +0900

    Float128 + libquadmath fix and macOS fix gcc10 build verified

--- misc/reconfig.macOS.sh
+++ misc/reconfig.macOS.sh
@@ -3,21 +3,21 @@
 USE_CCACHE=yes
 
 if [ x$USE_CCACHE = x"yes" ] ; then
-CXX="ccache g++-mp-9" ; export CXX
-CC="ccache gcc-mp-9" ; export CC
-FC="gfortran-mp-9"; export FC
-F77="gfortran-mp-9"; export F77
+CXX="ccache g++-mp-10" ; export CXX
+CC="ccache gcc-mp-10" ; export CC
+FC="gfortran-mp-10"; export FC
+F77="gfortran-mp-10"; export F77
 ccache -M 20G
 else
-CXX="g++-mp-9" ; export CXX
-CC="gcc-mp-9" ; export CC
-FC="gfortran-mp-9"; export FC
-F77="gfortran-mp-9"; export F77
+CXX="g++-mp-10" ; export CXX
+CC="gcc-mp-10" ; export CC
+FC="gfortran-mp-10"; export FC
+F77="gfortran-mp-10"; export F77
 fi
 
-pushd mplapack/debug ; bash gen.Makefile.am.sh ; popd
+pushd mplapack/test ; bash gen.Makefile.am.sh ; popd
 
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
 
-./configure --prefix=$HOME/MPLAPACK --with-system-gmp=/opt/local --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-debug=yes
+./configure --prefix=$HOME/MPLAPACK --with-system-gmp=/opt/local --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
