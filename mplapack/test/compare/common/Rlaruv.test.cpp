7cfab4c9b2dde6829fe1d1e867ccb8ea2f95e81c
NAKATA Maho <maho.nakata@gmail.com>
2021-04-25 02:41:30 +0000

    .

---
+++ mplapack/test/compare/common/Rlaruv.test.cpp
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2021
+ *	Nakata, Maho
+ * 	All rights reserved.
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+/*
+Copyright (c) 1992-2007 The University of Tennessee.  All rights reserved.
+ *
+ * $Id: Mutils.debug.cpp,v 1.13 2010/08/07 05:50:10 nakatamaho Exp $
+
+$COPYRIGHT$
+
+Additional copyrights may follow
+
+$HEADER$
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+- Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+- Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer listed
+  in this license in the documentation and/or other materials
+  provided with the distribution.
+
+- Neither the name of the copyright holders nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <mpblas.h>
+#include <mplapack.h>
+#include <mplapack_debug.h>
+#include <fstream>
+
+#include <blas.h>
+
+#if defined VERBOSE_TEST
+#include <iostream>
+#endif
+
+#define MAX_N 128
+
+#define outname "Rlaruv_mpfr.txt"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    mpreal::set_default_prec(___MPLAPACK_DEFAULT_PRECISION___);
+    mpcomplex::set_default_prec(___MPLAPACK_DEFAULT_PRECISION___);
+    printf("*** Testing Rlaruv start ***\n");
+    INTEGER *seed; // ignored
+    for (int n = 0; n < MAX_N; n++) {
+        printf("%d-th random vector\n", n);
+        REAL *x = new REAL[n];
+        Rlaruv(seed, n, x);
+        for (int i = 0; i < n; i++) {
+            printf("%d:", i);
+            printnum(x[i]);
+            printf("\n");
+        }
+        delete[] x;
+        printf("%d-th random vector done\n", n);
+    }
+    int n = 1024;
+    REAL *x = new REAL[n];
+    char *buf = new char[n * 1024];
+    ofstream outputfile(outname);
+    Rlaruv(seed, n, x);
+    for (int i = 0; i < n; i++) {
+        sprintnum(buf, x[i]);
+        outputfile << i << " " << buf << "\n";
+    }
+    delete[] x;
+    outputfile.close();
+    printf("*** Please check Rlaruv_mpfr.txt by plotting "gnuplot Rlaruv_mpfr.plt" ***\n");
+    printf("*** Testing Rlaruv successful ***\n");
+    return (0);
+}
