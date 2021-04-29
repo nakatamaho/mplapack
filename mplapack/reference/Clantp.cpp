275ddee9e10bc48e8693617e8be08488a4924da1
NAKATA Maho <maho.nakata@gmail.com>
2021-04-29 06:24:21 +0000

    .

---
+++ mplapack/reference/Clantp.cpp
@@ -0,0 +1,291 @@
+/*
+ * Copyright (c) 2008-2021
+ *      Nakata, Maho
+ *      All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <mpblas.h>
+#include <mplapack.h>
+
+REAL Clantp(const char *norm, const char *uplo, const char *diag, INTEGER const n, COMPLEX *ap, REAL *work) {
+    REAL return_value = 0.0;
+    //
+    //  -- LAPACK auxiliary routine --
+    //  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+    //  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+    //
+    //     .. Scalar Arguments ..
+    //     ..
+    //     .. Array Arguments ..
+    //     ..
+    //
+    // =====================================================================
+    //
+    //     .. Parameters ..
+    //     ..
+    //     .. Local Scalars ..
+    //     ..
+    //     .. Local Arrays ..
+    //     ..
+    //     .. External Functions ..
+    //     ..
+    //     .. External Subroutines ..
+    //     ..
+    //     .. Intrinsic Functions ..
+    //     ..
+    //     .. Executable Statements ..
+    //
+    const REAL zero = 0.0;
+    REAL value = 0.0;
+    INTEGER k = 0;
+    const REAL one = 1.0;
+    INTEGER j = 0;
+    INTEGER i = 0;
+    REAL sum = 0.0;
+    bool udiag = false;
+    REAL ssq[2];
+    REAL colssq[2];
+    if (n == 0) {
+        value = zero;
+    } else if (Mlsame(norm, "M")) {
+        //
+        //        Find max(abs(A(i,j))).
+        //
+        k = 1;
+        if (Mlsame(diag, "U")) {
+            value = one;
+            if (Mlsame(uplo, "U")) {
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = k; i <= k + j - 2; i = i + 1) {
+                        sum = abs(ap[i - 1]);
+                        if (value < sum || Risnan(sum)) {
+                            value = sum;
+                        }
+                    }
+                    k += j;
+                }
+            } else {
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = k + 1; i <= k + n - j; i = i + 1) {
+                        sum = abs(ap[i - 1]);
+                        if (value < sum || Risnan(sum)) {
+                            value = sum;
+                        }
+                    }
+                    k += n - j + 1;
+                }
+            }
+        } else {
+            value = zero;
+            if (Mlsame(uplo, "U")) {
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = k; i <= k + j - 1; i = i + 1) {
+                        sum = abs(ap[i - 1]);
+                        if (value < sum || Risnan(sum)) {
+                            value = sum;
+                        }
+                    }
+                    k += j;
+                }
+            } else {
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = k; i <= k + n - j; i = i + 1) {
+                        sum = abs(ap[i - 1]);
+                        if (value < sum || Risnan(sum)) {
+                            value = sum;
+                        }
+                    }
+                    k += n - j + 1;
+                }
+            }
+        }
+    } else if ((Mlsame(norm, "O")) || (normMlsame(, "1"))) {
+        //
+        //        Find norm1(A).
+        //
+        value = zero;
+        k = 1;
+        udiag = Mlsame(diag, "U");
+        if (Mlsame(uplo, "U")) {
+            for (j = 1; j <= n; j = j + 1) {
+                if (udiag) {
+                    sum = one;
+                    for (i = k; i <= k + j - 2; i = i + 1) {
+                        sum += abs(ap[i - 1]);
+                    }
+                } else {
+                    sum = zero;
+                    for (i = k; i <= k + j - 1; i = i + 1) {
+                        sum += abs(ap[i - 1]);
+                    }
+                }
+                k += j;
+                if (value < sum || Risnan(sum)) {
+                    value = sum;
+                }
+            }
+        } else {
+            for (j = 1; j <= n; j = j + 1) {
+                if (udiag) {
+                    sum = one;
+                    for (i = k + 1; i <= k + n - j; i = i + 1) {
+                        sum += abs(ap[i - 1]);
+                    }
+                } else {
+                    sum = zero;
+                    for (i = k; i <= k + n - j; i = i + 1) {
+                        sum += abs(ap[i - 1]);
+                    }
+                }
+                k += n - j + 1;
+                if (value < sum || Risnan(sum)) {
+                    value = sum;
+                }
+            }
+        }
+    } else if (Mlsame(norm, "I")) {
+        //
+        //        Find normI(A).
+        //
+        k = 1;
+        if (Mlsame(uplo, "U")) {
+            if (Mlsame(diag, "U")) {
+                for (i = 1; i <= n; i = i + 1) {
+                    work[i - 1] = one;
+                }
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = 1; i <= j - 1; i = i + 1) {
+                        work[i - 1] += abs(ap[k - 1]);
+                        k++;
+                    }
+                    k++;
+                }
+            } else {
+                for (i = 1; i <= n; i = i + 1) {
+                    work[i - 1] = zero;
+                }
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = 1; i <= j; i = i + 1) {
+                        work[i - 1] += abs(ap[k - 1]);
+                        k++;
+                    }
+                }
+            }
+        } else {
+            if (Mlsame(diag, "U")) {
+                for (i = 1; i <= n; i = i + 1) {
+                    work[i - 1] = one;
+                }
+                for (j = 1; j <= n; j = j + 1) {
+                    k++;
+                    for (i = j + 1; i <= n; i = i + 1) {
+                        work[i - 1] += abs(ap[k - 1]);
+                        k++;
+                    }
+                }
+            } else {
+                for (i = 1; i <= n; i = i + 1) {
+                    work[i - 1] = zero;
+                }
+                for (j = 1; j <= n; j = j + 1) {
+                    for (i = j; i <= n; i = i + 1) {
+                        work[i - 1] += abs(ap[k - 1]);
+                        k++;
+                    }
+                }
+            }
+        }
+        value = zero;
+        for (i = 1; i <= n; i = i + 1) {
+            sum = work[i - 1];
+            if (value < sum || Risnan(sum)) {
+                value = sum;
+            }
+        }
+    } else if ((Mlsame(norm, "F")) || (Mlsame(norm, "E"))) {
+        //
+        //        Find normF(A).
+        //        SSQ(1) is scale
+        //        SSQ(2) is sum-of-squares
+        //        For better accuracy, sum each column separately.
+        //
+        if (Mlsame(uplo, "U")) {
+            if (Mlsame(diag, "U")) {
+                ssq[1 - 1] = one;
+                ssq[2 - 1] = n;
+                k = 2;
+                for (j = 2; j <= n; j = j + 1) {
+                    colssq[1 - 1] = zero;
+                    colssq[2 - 1] = one;
+                    Classq(j - 1, &ap[k - 1], 1, colssq[1 - 1], colssq[2 - 1]);
+                    Rcombssq(ssq, colssq);
+                    k += j;
+                }
+            } else {
+                ssq[1 - 1] = zero;
+                ssq[2 - 1] = one;
+                k = 1;
+                for (j = 1; j <= n; j = j + 1) {
+                    colssq[1 - 1] = zero;
+                    colssq[2 - 1] = one;
+                    Classq(j, &ap[k - 1], 1, colssq[1 - 1], colssq[2 - 1]);
+                    Rcombssq(ssq, colssq);
+                    k += j;
+                }
+            }
+        } else {
+            if (Mlsame(diag, "U")) {
+                ssq[1 - 1] = one;
+                ssq[2 - 1] = n;
+                k = 2;
+                for (j = 1; j <= n - 1; j = j + 1) {
+                    colssq[1 - 1] = zero;
+                    colssq[2 - 1] = one;
+                    Classq(n - j, &ap[k - 1], 1, colssq[1 - 1], colssq[2 - 1]);
+                    Rcombssq(ssq, colssq);
+                    k += n - j + 1;
+                }
+            } else {
+                ssq[1 - 1] = zero;
+                ssq[2 - 1] = one;
+                k = 1;
+                for (j = 1; j <= n; j = j + 1) {
+                    colssq[1 - 1] = zero;
+                    colssq[2 - 1] = one;
+                    Classq(n - j + 1, &ap[k - 1], 1, colssq[1 - 1], colssq[2 - 1]);
+                    Rcombssq(ssq, colssq);
+                    k += n - j + 1;
+                }
+            }
+        }
+        value = ssq[1 - 1] * sqrt(ssq[2 - 1]);
+    }
+    //
+    return_value = value;
+    return return_value;
+    //
+    //     End of Clantp
+    //
+}
---
+++ mplapack/reference/Ctptri.cpp
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2008-2021
+ *      Nakata, Maho
+ *      All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <mpblas.h>
+#include <mplapack.h>
+
+void Ctptri(const char *uplo, const char *diag, INTEGER const n, COMPLEX *ap, INTEGER &info) {
+    //
+    //  -- LAPACK computational routine --
+    //  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+    //  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+    //
+    //     .. Scalar Arguments ..
+    //     ..
+    //     .. Array Arguments ..
+    //     ..
+    //
+    //  =====================================================================
+    //
+    //     .. Parameters ..
+    //     ..
+    //     .. Local Scalars ..
+    //     ..
+    //     .. External Functions ..
+    //     ..
+    //     .. External Subroutines ..
+    //     ..
+    //     .. Executable Statements ..
+    //
+    //     Test the input parameters.
+    //
+    info = 0;
+    bool upper = Mlsame(uplo, "U");
+    bool nounit = Mlsame(diag, "N");
+    if (!upper && !Mlsame(uplo, "L")) {
+        info = -1;
+    } else if (!nounit && !Mlsame(diag, "U")) {
+        info = -2;
+    } else if (n < 0) {
+        info = -3;
+    }
+    if (info != 0) {
+        Mxerbla("Ctptri", -info);
+        return;
+    }
+    //
+    //     Check for singularity if non-unit.
+    //
+    INTEGER jj = 0;
+    const COMPLEX zero = COMPLEX(0.0, 0.0);
+    if (nounit) {
+        if (upper) {
+            jj = 0;
+            for (info = 1; info <= n; info = info + 1) {
+                jj += info;
+                if (ap[jj - 1] == zero) {
+                    return;
+                }
+            }
+        } else {
+            jj = 1;
+            for (info = 1; info <= n; info = info + 1) {
+                if (ap[jj - 1] == zero) {
+                    return;
+                }
+                jj += n - info + 1;
+            }
+        }
+        info = 0;
+    }
+    //
+    INTEGER jc = 0;
+    INTEGER j = 0;
+    const COMPLEX one = COMPLEX(1.0, 0.0);
+    COMPLEX ajj = 0.0;
+    INTEGER jclast = 0;
+    if (upper) {
+        //
+        //        Compute inverse of upper triangular matrix.
+        //
+        jc = 1;
+        for (j = 1; j <= n; j = j + 1) {
+            if (nounit) {
+                ap[(jc + j - 1) - 1] = one / ap[(jc + j - 1) - 1];
+                ajj = -ap[(jc + j - 1) - 1];
+            } else {
+                ajj = -one;
+            }
+            //
+            //           Compute elements 1:j-1 of j-th column.
+            //
+            Ctpmv("Upper", "No transpose", diag, j - 1, ap, &ap[jc - 1], 1);
+            Cscal(j - 1, ajj, &ap[jc - 1], 1);
+            jc += j;
+        }
+        //
+    } else {
+        //
+        //        Compute inverse of lower triangular matrix.
+        //
+        jc = n * (n + 1) / 2;
+        for (j = n; j >= 1; j = j - 1) {
+            if (nounit) {
+                ap[jc - 1] = one / ap[jc - 1];
+                ajj = -ap[jc - 1];
+            } else {
+                ajj = -one;
+            }
+            if (j < n) {
+                //
+                //              Compute elements j+1:n of j-th column.
+                //
+                Ctpmv("Lower", "No transpose", diag, n - j, &ap[jclast - 1], &ap[(jc + 1) - 1], 1);
+                Cscal(n - j, ajj, &ap[(jc + 1) - 1], 1);
+            }
+            jclast = jc;
+            jc = jc - n + j - 2;
+        }
+    }
+    //
+    //     End of Ctptri
+    //
+}
