3892be32d61711ec9ce209cf57bf8ad096a1b0fa
NAKATA Maho <maho.nakata@gmail.com>
2021-06-17 21:49:38 +0900

    .

--- misc/debug_ideom.cpp
+++ misc/debug_ideom.cpp
@@ -1,3 +1,45 @@
+	{
+   	    double *t_d = new double [ldt * n];
+   	    double *q_d = new double [ldq * n];
+	    double s_d, sep_d;
+	    int m_d;
+	    double *wrtmp_d = new double[n];
+	    double *witmp_d = new double[n];
+	    int *select_d = new int[n];	    
+	    for ( int pp=1; pp<=n; pp++){
+	      for ( int qq=1; qq<= n; qq++){
+		t_d[ (pp -1) + (qq-1) * ldt ] = t[ (pp -1) + (qq-1) * ldt ];
+		q_d[ (pp -1) + (qq-1) * ldq ] = q[ (pp -1) + (qq-1) * ldt ];		
+	      }
+	    }
+  	    LAPACKE_dtrsen (LAPACK_COL_MAJOR, 'B', 'V', select_d, (int)n, t_d, (int)ldt, q_d, (int)ldt, wrtmp_d, witmp_d, &m_d, &s_d, &sep_d);
+	    printf("sep_d = "); printnum(sep_d); printf("\n");
+            delete [] select_d;
+            delete [] witmp_d;
+            delete [] wrtmp_d;
+            delete [] q_d;
+            delete [] t_d;
+	}
+
+
+/*
+	{
+          double *t_d = new double [n * n];
+          double *work_d = new double [lwork];
+          for ( int p=1; p<=n; p++){
+            for ( int q=1; q<= n; q++){
+              t_d[ (p -1) + (q-1) * ldt ] = t[ (p -1) + (q-1) * ldt ];
+            }
+          }
+	  printf("torg=");  printmat(n, n, t, ldt);  printf("\n");
+          LAPACKE_dgehrd(LAPACK_COL_MAJOR, (int)n, 1, n, t_d, (int)ldt, &work_d[1-1]);
+          printf("tout_d=");printmat(n,n,t_d,ldt);printf("\n");
+          delete [] t_d;
+          delete [] work_d;
+	}
+	*/
+
+
     __complex__ double *afac_d;
     __complex__ double *tau_d;
     {
@@ -50,4 +92,73 @@
     delete []work_d;
     delete []t2_d;
     delete []q_d;
+///////////////////////////////////////////////
+                                        printf("ca=");   printnum(ca);              printf("\n");
+                                        printf("a=");    printmat(na, na, a, lda);  printf("\n");
+                                        printf("b=");    printmat(na, nw, b, ldb);  printf("\n");
+                                        printf("w=");    printnum(wr);              printf("\n");
+                                        printf("d1=");   printnum(d1);              printf("\n");
+                                        printf("d2=");   printnum(d2);              printf("\n");
+                                        printf("s=");    printnum(scale);           printf("\n");
+                                        printf("X=");    printmat(na, 1, x, ldx);   printf("\n");
+                                        printf("\n");
+                                        printf("(ca * a - w * d) * X - (s * b)\n");
+                                        printf("d=diag([d1,d2])\n");
+                                        printf("info = %d\n", (int)info);
+                                        bool trans = ltrans[itrans - 1];
+                                        int na_d = (int)na;
+                                        int nw_d = (int)nw;
+                                        double smin_d = smin;
+                                        double ca_d = ca;
+                                        double a_d[4];
+                                        a_d[0] = a[0];
+                                        a_d[1] = a[1];
+                                        a_d[2] = a[2];
+                                        a_d[3] = a[3];
+                                        int lda_d = 2;
+                                        double d1_d = d1;
+                                        double d2_d = d2;
+                                        double b_d[4];
+                                        b_d[0] = b[0];
+                                        b_d[1] = b[1];
+                                        b_d[2] = b[2];
+                                        b_d[3] = b[3];
+                                        double x_d[4];
+                                        double wr_d = wr;
+                                        double wi_d = wi;
+                                        double scale_d;
+                                        double xnorm_d;
+                                        int info_d;
+                                        dlaln2_(&trans, &na_d, &nw_d, &smin_d, &ca_d, a_d, &lda_d, &d1_d, &d2_d, b_d, &lda_d, &wr_d, &wi_d, x_d, &lda_d, &scale_d, &xnorm_d, &info_d);
+                                        printf("ca_d=");   printnum(ca_d);              printf("\n");
+                                        printf("a_d=");    printmat(na, na, a_d, lda);  printf("\n");
+                                        printf("b_d=");    printmat(na, nw, b_d, ldb);  printf("\n");
+                                        printf("w_d=");    printnum(wr_d);              printf("\n");
+                                        printf("d1_d=");   printnum(d1_d);              printf("\n");
+                                        printf("d2_d=");   printnum(d2_d);              printf("\n");
+                                        printf("s_d=");    printnum(scale_d);           printf("\n");
+                                        printf("X_d=");    printmat(na, 1, x_d, ldx);   printf("\n");
+                                        printf("\n");
+///////////////////////////////////////////////
 
+
+	/*
+	  double *t_d = new double [ldt * ldt];
+	  double *le_d = new double [ldt * ldt];
+	  double *re_d = new double [ldt * ldt];
+	  int *select_d = new int [10];
+          int m_d;
+	  for (int p=1; p<=n; p++){
+            for (int q=1; q<= n; q++) {
+              t_d [ (p -1) + (q-1) * ldt ] = t[(p -1) + (q-1) * ldt];
+            }
+	  }
+	  printf("t_d="); printmat(n, n, t, ldt); printf("\n");
+	  LAPACKE_dtrevc(LAPACK_COL_MAJOR, 'B', 'A', select_d, (int)n, t_d, (int) ldt, le_d, ldt, re_d, (int)ldt, (int)n, &m_d);
+	  printf("vlout_d="); printmat(n, n, le_d, ldt); printf("\n");
+	  printf("vrout_d="); printmat(n, n, re_d, ldt); printf("\n");
+	  delete [] select_d;
+	  delete [] re_d;
+	  delete [] le_d;
+	  delete [] t_d;
+	*/
