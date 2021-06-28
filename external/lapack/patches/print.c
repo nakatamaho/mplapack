571954e013809b8aba4210f8561ad6fe80179c44
NAKATA Maho <maho.nakata@gmail.com>
2021-06-28 16:24:46 +0900

    .

--- external/lapack/patches/print.c
+++ external/lapack/patches/print.c
@@ -3,15 +3,12 @@
 
 char buf[1024];
 
-void printdummy_(){
- printf("\n");
-}
-
+void printdummy_() { printf("\n"); }
 
 void printnum_(char *s, int *len, double *A) {
-        memset(buf, 0, sizeof(buf));
-        strncpy(buf, s, *len);
-        printf("%s%+21.16e\n", buf, *A);
+    memset(buf, 0, sizeof(buf));
+    strncpy(buf, s, *len);
+    printf("%s%+21.16e\n", buf, *A);
 }
 
 void printmat_(char *s, int *len, int *N, int *M, double *A, int *LDA) {
@@ -19,32 +16,35 @@ void printmat_(char *s, int *len, int *N, int *M, double *A, int *LDA) {
     strncpy(buf, s, *len);
     double tmp;
     printf("%s[ ", buf);
-  for (int i = 0; i < *N; i++) {
+    for (int i = 0; i < *N; i++) {
         printf("[ ");
-    for (int j = 0; j < *M; j++) {
+        for (int j = 0; j < *M; j++) {
             tmp = A[i + j * (*LDA)];
-        printf("%+21.16e", tmp);
+            printf("%+21.16e", tmp);
             if (j < *M - 1)
                 printf(", ");
-    }
+        }
         if (i < *N - 1)
             printf("]; ");
         else
             printf("] ");
-  }
+    }
     printf("]");
     printf("\n");
 }
 
-void printvec_(double *A, int *len) {
+void printvec_(char *s, int *len, double *A, int *len) {
     double tmp;
-    printf("[ ");
-  for (int i = 0; i < *len; i++) {
+    memset(buf, 0, sizeof(buf));
+    strncpy(buf, s, *len);
+    double tmp;
+    printf("%s[ ", buf);
+    for (int i = 0; i < *len; i++) {
         tmp = A[i];
         printf("%+21.16e", tmp);
         if (i < *len - 1)
             printf(", ");
-  }
+    }
     printf("]");
     printf("\n");
 }
