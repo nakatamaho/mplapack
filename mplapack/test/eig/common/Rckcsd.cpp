dd69fed14979c0f4ebbf27752b3f86cef689e866
NAKATA Maho <maho.nakata@gmail.com>
2021-07-07 12:28:37 +0000

    .

--- mplapack/test/eig/common/Rckcsd.cpp
+++ mplapack/test/eig/common/Rckcsd.cpp
@@ -63,7 +63,8 @@ void Rckcsd(INTEGER const nm, INTEGER *mval, INTEGER *pval, INTEGER *qval, INTEG
     INTEGER iinfo = 0;
     INTEGER r = 0;
     INTEGER i = 0;
-    const REAL piover2 = 1.57079632679489661923132169163975144210e0;
+    REAL dummy;
+    const REAL piover2 = pi(dummy)/2.0;
     INTEGER j = 0;
     const REAL orth = 1.0e-12;
     const REAL ten = 10.0;
