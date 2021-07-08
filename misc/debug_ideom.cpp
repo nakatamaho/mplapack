8f98c25a818acad6fbcf3d5bf9af4c036f8743dc
NAKATA Maho <maho.nakata@gmail.com>
2021-07-08 03:37:07 +0000

    .

--- misc/debug_ideom.cpp
+++ misc/debug_ideom.cpp
@@ -1,3 +1,99 @@
+    {
+        double *xf_d = new double[max(m * m, (INTEGER)1)];
+        double *u1_d = new double[max(p * p, (INTEGER)1)];
+        double *u2_d = new double[max((m-p) * (m-p), (INTEGER)1)];
+        double *v1_d = new double[max(q * q, (INTEGER)1)];
+        double *v2_d = new double[max((m-q) * (m-q),(INTEGER)1)];	
+        double *theta_d = new double[max(r,(INTEGER)1)];
+	int ldx_d =max((int)m,1);
+	int ldu1_d=(int)p;
+	int ldu2_d=(int)(m-p);	
+	int ldv1_d=(int)q;
+	int ldv2_d=(int)(m-q);	
+	for (int ii=0;ii<m;ii++) {
+	    for (int jj=0;jj<m;jj++) {
+	      xf_d[ii+jj*ldx_d] = x[ii+jj*ldx];
+	    }
+	}
+        LAPACKE_dorcsd(LAPACK_COL_MAJOR, 'Y', 'Y', 'Y', 'Y', 'N', 'D', (int)m, (int)p, (int)q, xf_d, (int)ldx_d, &xf_d[(((int)q + 1) - 1) * ldx_d], (int)ldx_d, &xf_d[(((int)p + 1) - 1)], (int)ldx_d, &xf_d[(((int)p + 1) - 1) + (((int)q + 1) - 1) * ldx_d], (int)ldx_d, theta_d, u1_d, ldu1_d, u2_d, ldu2_d, v1_d, ldv1_d, v2_d, ldv2_d);
+	double dnorm;
+	double dtmp;
+	dnorm = 0.0;
+        for (int _i=0; _i < m; _i++) {
+	    for (int _j=0; _j < m; _j++) {
+	      dtmp = xf_d[_i + _j * m] -xf[_i + _j * ldx];
+	      dnorm = dnorm + fabs(dtmp);
+	    }
+	}
+	printf("diff (xf_d, xf)= %lf\n", dnorm);
+
+	dnorm = 0.0;
+        for (int _i=0; _i < p; _i++) {
+	    for (int _j=0; _j < p; _j++) {
+	      dtmp = u1_d[_i + _j * p] - u1[_i + _j * ldu1];
+	      dnorm = dnorm + fabs(dtmp);
+	    }
+	}
+	printf("diff (u1_d, u1)= %lf\n", dnorm);
+        if (dnorm >1.0 ) {
+            printf("u1_d=");printmat(p,p,u1_d,p);printf("\n");
+            printf("u1=");printmat(p,p,u1,ldu1);printf("\n");
+	}
+
+	dnorm = 0.0;
+        for (int _i=0; _i < m-p; _i++) {
+	    for (int _j=0; _j < m-p; _j++) {
+	      dtmp = u2_d[_i + _j * (m-p)] - u2[_i + _j * ldu2];
+	      dnorm = dnorm + fabs(dtmp);
+	    }
+	}
+	printf("diff (u2_d, u2)= %lf\n", dnorm);
+        if (dnorm >1.0 ) {
+            printf("u2_d=");printmat(m-p,m-p,u1_d,m-p);printf("\n");
+            printf("u2=");printmat(m-p,m-p,u1,ldu2);printf("\n");
+	}
+
+	dnorm = 0.0;
+        for (int _i=0; _i < q; _i++) {
+	    for (int _j=0; _j <q; _j++) {
+	      dtmp = v1_d[_i + _j * q] - v1t[_i + _j * ldv1t];
+	      dnorm = dnorm + fabs(dtmp);
+	    }
+	}
+	printf("diff (v1_d, v1)= %lf\n", dnorm);	
+        if (dnorm >1.0 ) {
+            printf("v1_d=");printmat(q,q,v1_d,q);printf("\n");
+            printf("v1=");printmat(q,q,v1t,ldv1t);printf("\n");
+	}
+	
+	dnorm = 0.0;
+        for (int _i=0; _i < m-q; _i++) {
+	    for (int _j=0; _j < m-q; _j++) {
+	      dtmp = v2_d[_i + _j * (m-q)] - v2t[_i + _j * ldv2t];
+	      dnorm = dnorm + fabs(dtmp);
+	    }
+	}
+	printf("diff (v2_d, v2)= %lf\n", dnorm);
+
+        dnorm = 0.0;
+        for (int _i=0; _i < r; _i++) {
+	      dtmp = theta_d[_i] - theta[_i];
+	      dnorm = dnorm + fabs(dtmp);
+	}
+	printf("diff (theta_d, theta)= %lf\n", dnorm);
+        if (dnorm >1.0 ) {
+	  printf("theta_d=");printvec(theta_d,r);printf("\n");
+	  printf("theta=");printvec(theta, r);printf("\n");
+	}
+	delete[] theta_d;
+        delete[] v2_d;
+        delete[] v1_d;
+        delete[] u2_d;
+        delete[] u1_d;
+        delete[] xf_d;
+    }
+    printf("\n");
+
                 REAL *a_org = new REAL[n * n];
                 REAL *h_org = new REAL[n * n];
                 for (int pp = 0; pp < n; pp++) {
