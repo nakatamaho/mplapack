6989b60e08efbc2771c314e6a8142569826b3481
NAKATA Maho <maho.nakata@gmail.com>
2021-08-09 06:28:54 +0900

    add more example

---
+++ examples/mplapack/04_NonsymmetricEigenproblems/Rgeev_test_mpfr.cpp
@@ -0,0 +1,70 @@
+// Public domain
+#include <mpblas_mpfr.h>
+#include <mplapack_mpfr.h>
+
+#define MPFR_FORMAT "%+68.64Re"
+#define MPFR_SHORT_FORMAT "%+20.16Re"
+
+inline void printnum(mpreal rtmp) { mpfr_printf(MPFR_FORMAT, mpfr_ptr(rtmp)); }
+inline void printnum_short(mpreal rtmp) { mpfr_printf(MPFR_SHORT_FORMAT, mpfr_ptr(rtmp)); }
+
+// Matlab/Octave format
+void printmat(int N, int M, mpreal *A, int LDA) {
+    mpreal mtmp;
+    printf("[ ");
+    for (int i = 0; i < N; i++) {
+        printf("[ ");
+        for (int j = 0; j < M; j++) {
+            mtmp = A[i + j * LDA];
+            printnum_short(mtmp);
+            if (j < M - 1)
+                printf(", ");
+        }
+        if (i < N - 1)
+            printf("]; ");
+        else
+            printf("] ");
+    }
+    printf("]");
+}
+
+bool rselect(mpreal ar, mpreal ai) {
+    // sorting rule for eigenvalues.
+    return false;
+}
+
+int main() {
+    mplapackint n = 4;
+    mpreal *a = new mpreal[n * n];
+    mpreal *vl = new mpreal[n * n];
+    mpreal *vr = new mpreal[n * n];
+    mplapackint lwork = 4 * n;
+    mpreal *wr = new mpreal[n];
+    mpreal *wi = new mpreal[n];
+    mpreal *work = new mpreal[lwork];
+    mplapackint info;
+    // setting A matrix
+    a[0 + 0 * n] = 0.35;     a[0 + 1 * n] = -0.1160;   a[0 + 2 * n] = -0.3886;    a[0 + 3 * n] = -0.2942;
+    a[1 + 0 * n] = -0.5140;  a[1 + 1 * n] = 0.1225;    a[1 + 2 * n] = 0.1004;     a[1 + 3 * n] = 0.1126;
+    a[2 + 0 * n] = 0.0;      a[2 + 1 * n] = 0.6443;    a[2 + 2 * n] = -0.1357;    a[2 + 3 * n] = -0.0977;
+    a[3 + 0 * n] = -3.0;     a[3 + 1 * n] = 0.0;       a[3 + 2 * n] = 0.4262;     a[3 + 3 * n] = 0.1632;
+
+    printf("# octave check\n");
+    printf("split_long_rows(0)\n");
+    printf("a ="); printmat(n, n, a, n); printf("\n");
+    Rgeev("V", "V", n, a, n, wr, wi, vl, n, vr, n, work, lwork, info);
+    printf("# left vectors\n");
+    printf("vl ="); printmat(n, n, vl, n); printf("\n");
+    printf("# left vectors\n");
+    printf("vr ="); printmat(n, n, vr, n); printf("\n");
+    printf("[vl, d, w] = eig(a)\n");
+    for (int i = 1; i <= n; i = i + 1) {
+        printf("w_%d = ", (int)i); printnum_short(wr[i - 1]); printf(" "); printnum_short(wi[i - 1]); printf("i\n");
+    }
+    delete[] work;
+    delete[] wr;
+    delete[] wi;
+    delete[] vr;
+    delete[] vl;
+    delete[] a;
+}
