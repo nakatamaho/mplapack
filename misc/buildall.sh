3592f9eb170083b81aea93755eab08b8e8223513
NAKATA Maho <maho.nakata@gmail.com>
2021-08-23 23:28:54 +0900

    .

--- misc/buildall.sh
+++ misc/buildall.sh
@@ -1,31 +1,14 @@
+docker buildx build --platform linux/arm64   -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004aarch64 2>&1 | tee log.ubuntu2004aarch64 
+docker buildx build --platform linux/arm64   -f Dockerfile_CentOS7_AArch64 . -t mplapack:centos7aarch64    2>&1 | tee log.centos7aarch64   
+docker buildx build --platform linux/arm64   -f Dockerfile_CentOS8         . -t mplapack:centos8aarch64    2>&1 | tee log.centos8aarch64   
+docker buildx build --platform linux/ppc64le -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004ppc64le 2>&1 | tee log.ubuntu2004ppc64le
+docker build -f  Dockerfile_CentOS7 .             -t mplapack:centos7               2>&1 | tee log.centos7
+docker build -f  Dockerfile_CentOS8 .             -t mplapack:centos8               2>&1 | tee log.centos8
+docker build -f  Dockerfile_fable .               -t mplapack:fable		    2>&1 | tee log.fable
+docker build -f  Dockerfile_ubuntu18.04  .        -t mplapack:ubuntu1804            2>&1 | tee log.ubuntu1804
+docker build -f  Dockerfile_ubuntu20.04  .        -t mplapack:ubuntu2004            2>&1 | tee log.ubuntu2004
+docker build -f  Dockerfile_ubuntu20.04_intel .   -t mplapack:ubuntu2004intel       2>&1 | tee log.ubuntu2004intel
+docker build -f  Dockerfile_ubuntu20.04_mingw64 . -t mplapack:ubuntu2004mingw64     2>&1 | tee log.ubuntu2004mingw64 
 
-MPLAPACKVER=0.8.0
-rm -rf build
-mkdir build
-cd build
 
-mkdir gcc
-cd gcc
-md5sum ~/mplapack-$MPLAPACKVER.tar.gz
-tar xvfz ~/mplapack-$MPLAPACKVER.tar.gz
-cd mplapack-$MPLAPACKVER
-./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-_Float128=yes --prefix=/home/docker/MPLAPACK.GCC
-/usr/bin/time make -j20
-/usr/bin/time make install
-cd ..
-cd ..
 
-mkdir icc
-cd icc
-md5sum ~/mplapack-$MPLAPACKVER.tar.gz
-tar xvfz ~/mplapack-$MPLAPACKVER.tar.gz
-cd mplapack-$MPLAPACKVER
-source /opt/intel/composer_xe_2013/bin/compilervars.sh intel64
-export CC="icc"
-export CXX="icpc"
-export FC="ifort"
-./configure --enable-optimization=yes --enable-debug=yes --enable-mpfr=yes --enable-qd=yes --enable-double=yes --enable-_Float128=yes --prefix=/home/docker/MPLAPACK.ICC --with-external-blas="-mkl" --with-external-lapack="-mkl"
-/usr/bin/time make -j20
-/usr/bin/time make install
-cd ..
-cd ..
