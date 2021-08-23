8a999f13b0f165b2fb96bc1f1eb6f4fb3a947ac5
NAKATA Maho <maho.nakata@gmail.com>
2021-08-23 22:52:38 +0900

    .

--- misc/buildtest.sh
+++ misc/buildtest.sh
@@ -1,7 +1,7 @@
-docker build  -f Dockerfile_CentOS7                -t mplapack:centos7           . 2>&1 | tee log.centos7
-docker build  -f Dockerfile_CentOS8                -t mplapack:centos8           . 2>&1 | tee log.centos8
-docker build  -f Dockerfile_ubuntu18.04            -t mplapack:ubuntu1804        . 2>&1 | tee log.ubuntu1804
-docker build  -f Dockerfile_ubuntu20.04            -t mplapack:ubuntu2004        . 2>&1 | tee log.ubuntu2004
-docker build  -f Dockerfile_ubuntu20.04_intel      -t mplapack:ubuntu2004intel   . 2>&1 | tee log.ubuntu2004intel
-docker build  -f Dockerfile_ubuntu20.04_mingw64    -t mplapack:ubuntu2004mingw64 . 2>&1 | tee log.ubuntu2004mingw64
-#docker buildx -f Dockerfile_ubuntu20.04            -t mplapack:ubuntu2004ppc64le . 2>&1 | tee log.ubuntu2004ppc64le
+TAGS="centos8 centos7 ubuntu2004 ubuntu2004intel ubuntu2004mingw64 ubuntu1804 ubuntu2004ppc64le centos8aarch64 centos7aarch64 ubuntu2004aarch64"
+for _tag in $TAGS; do
+    rm -f a
+    docker run --cidfile="a" -u docker -it mplapack:${_tag} bash -x /home/docker/a.sh 2>&1 | tee log.${_tag}
+    container_id=`cat a`
+    docker commit $container_id mplapack:${_tag}
+done
--- misc/slimup_dockerimages.sh
+++ misc/slimup_dockerimages.sh
@@ -1,4 +1,4 @@
-TAGS="mplapack:ubuntu2004 mplapack:centos8 mplapack:centos7 mplapack:ubuntu2004ppc64le mplapack:ubuntu2004intel mplapack:ubuntu2004mingw64 mplapack:ubuntu1804"
+TAGS="centos8 centos7 ubuntu2004 ubuntu2004intel ubuntu2004mingw64 ubuntu1804 ubuntu2004ppc64le centos8aarch64 centos7aarch64 ubuntu2004aarch64"
 for _tag in $TAGS; do 
     rm -f a
     docker run --cidfile="a" $_tag ccache -C
