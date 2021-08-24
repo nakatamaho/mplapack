a2ac480320d19902b9c2bb339b9c180b1af8fabf
NAKATA Maho <maho.nakata@gmail.com>
2021-08-25 08:35:03 +0900

    .

--- misc/buildall.sh
+++ misc/buildall.sh
@@ -1,7 +1,3 @@
-docker buildx build --platform linux/arm64   -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004aarch64 --load 2>&1 | tee log.ubuntu2004aarch64 
-docker buildx build --platform linux/arm64   -f Dockerfile_CentOS7_AArch64 . -t mplapack:centos7aarch64    --load 2>&1 | tee log.centos7aarch64   
-docker buildx build --platform linux/arm64   -f Dockerfile_CentOS8         . -t mplapack:centos8aarch64    --load 2>&1 | tee log.centos8aarch64   
-docker buildx build --platform linux/ppc64le -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004ppc64le --load 2>&1 | tee log.ubuntu2004ppc64le
 docker build -f  Dockerfile_CentOS7 .             -t mplapack:centos7               2>&1 | tee log.centos7
 docker build -f  Dockerfile_CentOS8 .             -t mplapack:centos8               2>&1 | tee log.centos8
 docker build -f  Dockerfile_fable .               -t mplapack:fable		    2>&1 | tee log.fable
@@ -10,5 +6,6 @@ docker build -f  Dockerfile_ubuntu20.04  .        -t mplapack:ubuntu2004        
 docker build -f  Dockerfile_ubuntu20.04_intel .   -t mplapack:ubuntu2004intel       2>&1 | tee log.ubuntu2004intel
 docker build -f  Dockerfile_ubuntu20.04_mingw64 . -t mplapack:ubuntu2004mingw64     2>&1 | tee log.ubuntu2004mingw64 
 
-
-
+docker buildx build --platform linux/arm64   -f Dockerfile_CentOS7_AArch64 . -t mplapack:centos7aarch64    --load 2>&1 | tee log.centos7aarch64   
+docker buildx build --platform linux/arm64   -f Dockerfile_CentOS8         . -t mplapack:centos8aarch64    --load 2>&1 | tee log.centos8aarch64   
+docker buildx build --platform linux/ppc64le -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004ppc64le --load 2>&1 | tee log.ubuntu2004ppc64le
