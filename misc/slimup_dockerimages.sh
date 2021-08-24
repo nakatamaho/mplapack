f8f351493415a96f53b555196a1d78831ff1ae20
NAKATA Maho <maho.nakata@gmail.com>
2021-08-25 08:35:25 +0900

    .

--- misc/slimup_dockerimages.sh
+++ misc/slimup_dockerimages.sh
@@ -1,10 +1,10 @@
 TAGS="centos8 centos7 ubuntu2004 ubuntu2004intel ubuntu2004mingw64 ubuntu1804 ubuntu2004ppc64le centos8aarch64 centos7aarch64 ubuntu2004aarch64"
 for _tag in $TAGS; do 
     rm -f a
-    docker run --cidfile="a" $_tag echo #ccache -C
+    docker run --cidfile="a" mplapack:$_tag echo
     container_id=`cat a`
     echo $container_id
-    docker export $container_id | gzip > ${_tag}.tar.gz ; cat ${_tag}.tar.gz | gunzip | docker import - ${_tag}
+    docker export $container_id | gzip > mplapack:${_tag}.tar.gz ; cat mplapack:${_tag}.tar.gz | gunzip | docker import - mplapack:${_tag}
     rm -f a
 done
 
