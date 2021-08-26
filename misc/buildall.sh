1e7d93078dcdfbd94b92ff410c6ce1853d865932
NAKATA Maho <maho.nakata@gmail.com>
2021-08-26 09:07:01 +0900

    add more targets

--- misc/buildall.sh
+++ misc/buildall.sh
@@ -9,3 +9,4 @@ docker build -f  Dockerfile_ubuntu20.04_mingw64 . -t mplapack:ubuntu2004mingw64 
 docker buildx build --platform linux/arm64   -f Dockerfile_CentOS7_AArch64 . -t mplapack:centos7aarch64    --load 2>&1 | tee log.centos7aarch64   
 docker buildx build --platform linux/arm64   -f Dockerfile_CentOS8         . -t mplapack:centos8aarch64    --load 2>&1 | tee log.centos8aarch64   
 docker buildx build --platform linux/ppc64le -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004ppc64le --load 2>&1 | tee log.ubuntu2004ppc64le
+docker buildx build --platform linux/riscv64 -f Dockerfile_ubuntu20.04     . -t mplapack:ubuntu2004riscv64 --load 2>&1 | tee log.ubuntu2004riscv64
--- misc/buildtest.sh
+++ misc/buildtest.sh
@@ -1,4 +1,4 @@
-TAGS="centos8 centos7 ubuntu2004 ubuntu2004intel ubuntu2004mingw64 ubuntu1804 ubuntu2004ppc64le centos8aarch64 centos7aarch64 ubuntu2004aarch64"
+TAGS="centos8 centos7 ubuntu2004 ubuntu2004intel ubuntu2004mingw64 ubuntu1804 centos8aarch64 centos7aarch64 ubuntu2004aarch64 ubuntu2004ppc64le ubuntu2004riscv64"
 for _tag in $TAGS; do
     rm -f a
     docker run --cidfile="a" -u docker -it mplapack:${_tag} bash -x /home/docker/a.sh 2>&1 | tee log.${_tag}
