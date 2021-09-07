f5a6b335e949e39ccef04ba23c25e9842c3b9725
NAKATA Maho <maho.nakata@gmail.com>
2021-09-07 13:26:53 +0900

    small fixes

--- examples/mplapack/05_SingularValueDecomposition/Rgesvd_random_highcond_gmp.cpp
+++ examples/mplapack/05_SingularValueDecomposition/Rgesvd_random_highcond_gmp.cpp
@@ -213,6 +213,7 @@ int main(int argc, char *argv[]) {
     }
     // 7. eig(A^t A).
     printf("at_a ="); printmat(n, n, at_a, n); printf("\n");
+    printf("eig(at_a)\n");
     mpf_class rtmp = 0.0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
