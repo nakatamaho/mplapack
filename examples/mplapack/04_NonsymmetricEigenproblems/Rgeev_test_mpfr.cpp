dff3f96cfc44d1f9d59fe56048262b7ff0782b88
NAKATA Maho <maho.nakata@gmail.com>
2021-09-24 07:36:07 +0900

    update example

--- examples/mplapack/04_NonsymmetricEigenproblems/Rgeev_test_mpfr.cpp
+++ examples/mplapack/04_NonsymmetricEigenproblems/Rgeev_test_mpfr.cpp
@@ -62,23 +62,84 @@ int main() {
     mpreal *work = new mpreal[lwork];
     mplapackint info;
     // setting A matrix
-    a[0 + 0 * n] = 0.35;     a[0 + 1 * n] = -0.1160;   a[0 + 2 * n] = -0.3886;    a[0 + 3 * n] = -0.2942;
-    a[1 + 0 * n] = -0.5140;  a[1 + 1 * n] = 0.1225;    a[1 + 2 * n] = 0.1004;     a[1 + 3 * n] = 0.1126;
-    a[2 + 0 * n] = 0.0;      a[2 + 1 * n] = 0.6443;    a[2 + 2 * n] = -0.1357;    a[2 + 3 * n] = -0.0977;
-    a[3 + 0 * n] = -3.0;     a[3 + 1 * n] = 0.0;       a[3 + 2 * n] = 0.4262;     a[3 + 3 * n] = 0.1632;
+    a[0 + 0 * n] = 4.0; a[0 + 1 * n] = -5.0; a[0 + 2 * n] =  0.0;  a[0 + 3 * n] =  3.0;
+    a[1 + 0 * n] = 0.0; a[1 + 1 * n] =  4.0; a[1 + 2 * n] = -3.0;  a[1 + 3 * n] = -5.0;
+    a[2 + 0 * n] = 5.0; a[2 + 1 * n] = -3.0; a[2 + 2 * n] =  4.0;  a[2 + 3 * n] =  0.0;
+    a[3 + 0 * n] = 3.0; a[3 + 1 * n] =  0.0; a[3 + 2 * n] =  5.0;  a[3 + 3 * n] =  4.0;
 
     printf("# octave check\n");
     printf("split_long_rows(0)\n");
-    printf("a ="); printmat(n, n, a, n); printf("\n");
+    printf("a =");
+    printmat(n, n, a, n);
+    printf("\n");
     Rgeev("V", "V", n, a, n, wr, wi, vl, n, vr, n, work, lwork, info);
+    printf("# right vectors\n");
+    for (int j = 1; j <= n; j = j + 1) {
+        if (abs(wi[j - 1]) < 1e-15) {
+	    printf("vr_%d =[ ", j);
+            for (int i = 1; i <= n - 1; i = i + 1) {
+                printnum(vr[(i - 1) + (j - 1) * n]); printf(", ");
+            }
+            printnum(vr[(n - 1) + (j - 1) * n]); printf("];\n");
+        } else {
+	    printf("vr_%d =[ ", j);
+            for (int i = 1; i <= n - 1; i = i + 1) {
+                printnum(vr[(i - 1) + (j - 1) * n]); printnum(-vr[(i - 1) + j * n]); printf("i, ");
+            }
+            printnum(vr[(n - 1) + (j - 1) * n]); printnum(-vr[(n - 1) + j * n]); printf("i ];\n");
+            printf("vr_%d =[ ", j + 1);
+            for (int i = 1; i <= n - 1; i = i + 1) {
+                printnum(vr[(i - 1) + (j - 1) * n]); printnum(vr[(i - 1) + j * n]); printf("i, ");
+            }
+            printnum(vr[(n - 1) + (j - 1) * n]); printnum(vr[(n - 1) + j * n]); printf("i ];\n");
+            j++; 
+	}
+    }
     printf("# left vectors\n");
-    printf("vl ="); printmat(n, n, vl, n); printf("\n");
-    printf("# left vectors\n");
-    printf("vr ="); printmat(n, n, vr, n); printf("\n");
-    printf("[vl, d, w] = eig(a)\n");
+    for (int j = 1; j <= n; j = j + 1) {
+        if (abs(wi[j - 1]) < 1e-15) {
+	    printf("vl_%d =[ ", j);
+            for (int i = 1; i <= n - 1; i = i + 1) {
+                printnum(vl[(i - 1) + (j - 1) * n]); printf(", ");
+            }
+            printnum(vl[(n - 1) + (j - 1) * n]); printf("];\n");
+        } else {
+	    printf("vl_%d =[ ", j);
+            for (int i = 1; i <= n - 1; i = i + 1) {
+                printnum(vl[(i - 1) + (j - 1) * n]); printnum(-vl[(i - 1) + j * n]); printf("i, ");
+            }
+            printnum(vl[(n - 1) + (j - 1) * n]); printnum(-vl[(n - 1) + j * n]); printf("i ];\n");
+            printf("vl_%d =[ ", j + 1);
+            for (int i = 1; i <= n - 1; i = i + 1) {
+                printnum(vl[(i - 1) + (j - 1) * n]); printnum(vl[(i - 1) + j * n]); printf("i, ");
+            }
+            printnum(vl[(n - 1) + (j - 1) * n]); printnum(vl[(n - 1) + j * n]); printf("i ];\n");
+            j++; 
+	}
+    }
+
     for (int i = 1; i <= n; i = i + 1) {
         printf("w_%d = ", (int)i); printnum(wr[i - 1]); printf(" "); printnum(wi[i - 1]); printf("i\n");
     }
+
+    for (int i = 1; i <= n; i = i + 1) {
+        printf("disp (\"a * vr_%d\")\n", i);
+        printf("a * vr_%d'\n", i);
+        printf("disp (\"w_%d * vr_%d\")\n", i, i);
+        printf("w_%d * vr_%d'\n", i, i);
+        printf("disp (\"vr_%d\")\n", i);
+        printf("vr_%d'\n", i);
+    }
+
+    for (int i = 1; i <= n; i = i + 1) {
+        printf("disp (\"vl_%d * a \")\n", i);
+        printf("vl_%d * a\n", i);
+        printf("disp (\"w_%d * vl_%d \")\n", i, i);
+        printf("w_%d * vl_%d\n", i, i);
+        printf("disp (\"vl_%d\")\n", i);
+        printf("vl_%d\n", i);
+    }
+
     delete[] work;
     delete[] wr;
     delete[] wi;
