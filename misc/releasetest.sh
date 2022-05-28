f4cd3f64b607de2795a574d3d02babc07c487d52
NAKATA Maho <maho.nakata@gmail.com>
2022-05-28 14:14:39 +0900

    .

--- misc/releasetest.sh
+++ misc/releasetest.sh
@@ -5,6 +5,7 @@ cd $TOPDIR
 pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 make dist
 rm -rf $TOPTDIR/tmp
 cd $TOPTDIR/tmp
