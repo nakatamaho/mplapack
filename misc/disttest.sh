c5cb96bdb3abff709c447db80c78bdcf50e73bed
NAKATA Maho <maho.nakata@gmail.com>
2022-05-28 14:37:10 +0900

    .

--- misc/disttest.sh
+++ misc/disttest.sh
@@ -1,15 +1,17 @@
 TOPDIR=/home/docker/mplapack
 VERSION=2.0.0
-
+####################
 cd $TOPDIR
 pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
 ./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 make dist-xz
-rm -rf $TOPTDIR/tmp
-cd $TOPTDIR/tmp
+####################
+rm -rf $TOPDIR/tmp
+cd $TOPDIR/tmp
 cp $TOPDIR/mplapack-${VERSION}.tar.xz .
-tar xvfz mplapack-${VERSION}.tar.xz
+tar xvf mplapack-${VERSION}.tar.xz
+cd mplapack-${VERSION}
 ./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 
