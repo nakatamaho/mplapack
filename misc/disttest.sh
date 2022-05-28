19c782e90005cb37aca194c7a7e4014404371c89
NAKATA Maho <maho.nakata@gmail.com>
2022-05-28 14:46:44 +0900

    .

--- misc/disttest.sh
+++ misc/disttest.sh
@@ -9,9 +9,11 @@ autoreconf --force --install
 make dist-xz
 ####################
 rm -rf $TOPDIR/tmp
+rm -rf $HOME/MPLAPACK
 cd $TOPDIR/tmp
 cp $TOPDIR/mplapack-${VERSION}.tar.xz .
 tar xvf mplapack-${VERSION}.tar.xz
 cd mplapack-${VERSION}
 ./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
-
+make -j`getconf _NPROCESSORS_ONLN`
+make install
