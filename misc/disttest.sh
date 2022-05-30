bd85880296f7fdceb5f5573ec4f302e645e066ec
NAKATA Maho <maho.nakata@gmail.com>
2022-05-30 13:43:07 +0900

    partial commit. making distfile as mplapack-2.0.0.tar.xz

--- misc/disttest.sh
+++ misc/disttest.sh
@@ -10,10 +10,15 @@ make dist-xz
 ####################
 rm -rf $TOPDIR/tmp
 rm -rf $HOME/MPLAPACK
+mkdir $TOPDIR/tmp
 cd $TOPDIR/tmp
+rm mplapack-${VERSION}.tar.xz
 cp $TOPDIR/mplapack-${VERSION}.tar.xz .
 tar xvf mplapack-${VERSION}.tar.xz
 cd mplapack-${VERSION}
+CC='ccache gcc'
+CXX='ccache g++'
+FC='ccache gfortran'
 ./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 make -j`getconf _NPROCESSORS_ONLN`
 make install
