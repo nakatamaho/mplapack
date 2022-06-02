0c7b8d02d2e955a8191cbabb3d6e6180047c4bca
Nakata Maho <maho.nakata@gmail.com>
2022-06-02 22:10:52 +0900

    now we use stock gcc to compile GMP. FSF GCC doesn't compile properly. see https://gmplib.org/list-archives/gmp-bugs/2008-February/000930.html the situation has not been changed since then.

---
+++ misc/disttest_mac.sh
@@ -0,0 +1,24 @@
+TOPDIR=/Volumes/Users/maho/tmp/mplapack
+VERSION=2.0.0
+####################
+cd $TOPDIR
+pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
+aclocal ; autoconf ; automake --add-missing
+autoreconf --force --install
+export CC='ccache gcc-mp-10'
+export CXX='ccache g++-mp-10'
+export FC='gfortran-mp-10'
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
+make dist-xz
+####################
+rm -rf $TOPDIR/tmp
+rm -rf $HOME/MPLAPACK
+mkdir $TOPDIR/tmp
+cd $TOPDIR/tmp
+rm mplapack-${VERSION}.tar.xz
+cp $TOPDIR/mplapack-${VERSION}.tar.xz .
+tar xvfJ mplapack-${VERSION}.tar.xz
+cd mplapack-${VERSION}
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
+make -j4
+make install
