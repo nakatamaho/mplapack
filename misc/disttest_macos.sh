861ce113ba5767e686616157c9ed1244f2f01167
NAKATA Maho <maho.nakata@gmail.com>
2022-06-03 15:35:49 +0900

    support for macos and partial support for mingw

--- misc/disttest_macos.sh
+++ misc/disttest_macos.sh
@@ -5,10 +5,7 @@ cd $TOPDIR
 pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
-export CC='ccache gcc-mp-10'
-export CXX='ccache g++-mp-10'
-export FC='gfortran-mp-10'
-./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
+./configure
 make dist-xz
 ####################
 rm -rf $TOPDIR/tmp
@@ -19,6 +16,11 @@ rm mplapack-${VERSION}.tar.xz
 cp $TOPDIR/mplapack-${VERSION}.tar.xz .
 tar xvfJ mplapack-${VERSION}.tar.xz
 cd mplapack-${VERSION}
+
+CXX="ccache g++-mp-10" ; export CXX
+CC="ccache gcc-mp-10" ; export CC
+FC="ccache gfortran-mp-10"; export FC
+
 ./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 make -j4
 make install
