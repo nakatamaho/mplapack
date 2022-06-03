417277fe60e7c90a2669bac48d30b9197e699279
NAKATA Maho <maho.nakata@gmail.com>
2022-06-03 22:31:45 +0900

    .

--- misc/disttest_mingw.sh
+++ misc/disttest_mingw.sh
@@ -5,7 +5,7 @@ cd $TOPDIR
 pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
-./configure
+./configure --prefix=$HOME/MPLAPACK --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 make dist-xz
 ####################
 rm -rf $TOPDIR/tmp
