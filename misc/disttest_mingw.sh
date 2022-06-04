a20e40f91c113587d6a986f4054bb28e70b8e624
NAKATA Maho <maho.nakata@gmail.com>
2022-06-04 21:15:02 +0900

    .

--- misc/disttest_mingw.sh
+++ misc/disttest_mingw.sh
@@ -16,7 +16,7 @@ rm mplapack-${VERSION}.tar.xz
 cp $TOPDIR/mplapack-${VERSION}.tar.xz .
 tar xvf mplapack-${VERSION}.tar.xz
 cd mplapack-${VERSION}
-
+#
 CXX="ccache x86_64-w64-mingw32-g++" ; export CXX
 CC="ccache x86_64-w64-mingw32-gcc" ; export CC
 FC="x86_64-w64-mingw32-gfortran"; export FC
@@ -26,6 +26,14 @@ RANLIB="x86_64-w64-mingw32-ranlib" ; export RANLIB
 AR="x86_64-w64-mingw32-ar" ; export AR
 export WINEPATH="/usr/x86_64-w64-mingw32/lib/;/usr/lib/gcc/x86_64-w64-mingw32/9.3-win32/;/usr/lib/gcc/x86_64-w64-mingw32/9.3-posix"
 #
+pkill -9 Xvfb
+export DISPLAY=:0.0
+Xvfb :0 -screen 0 1024x768x16 &
+sleep 3
+#
+rm -rf ~/.wine
+mkdir -p ~/.wine
+#
 ./configure --prefix=$HOME/MPLAPACK --host=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
-make -j`getconf _NPROCESSORS_ONLN`
+make
 make install
