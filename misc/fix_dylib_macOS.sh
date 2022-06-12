73994b8ca7d44c22200e9a9a815ec09665b49ae4
Nakata Maho <maho.nakata@gmail.com>
2022-06-13 04:13:05 +0900

    finally fixed dylib issue for macOS.

--- misc/fix_dylib_macOS.sh
+++ misc/fix_dylib_macOS.sh
@@ -7,14 +7,14 @@ cd $DIR
 FILES=`ls`
 for _file in $FILES; do
     echo $_file
-    otool -L $_file | grep "mplapack/external/mpc/../i/MPC/lib/libmpc"
+    otool -L $_file | grep "external/mpc/../i/MPC/lib/libmpc"
     if [ $? = 0 ]; then
         ORG=`otool -L $_file  | grep MPC | awk '{print $1}'` ;\
         _FIX=`otool -L $_file | grep MPC | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
         echo "install_name_tool -change $ORG $FIX $_file" ;\
         install_name_tool -change $ORG $FIX $_file
     fi
-    otool -L $_file | grep "mplapack/external/mpfr/../i/MPFR/lib/libmpfr"
+    otool -L $_file | grep "external/mpfr/../i/MPFR/lib/libmpfr"
     if [ $? = 0 ]; then
         ORG=`otool -L $_file  | grep MPFR | awk '{print $1}'` ;\
         _FIX=`otool -L $_file | grep MPFR | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
@@ -35,7 +35,7 @@ for _file in $FILES; do
         echo "install_name_tool -change $ORG $FIX $_file" ;\
         install_name_tool -change $ORG $FIX $_file
     fi
-    otool -L $_file | grep "mplapack/external/qd/../i/QD/lib/libqd"
+    otool -L $_file | grep "external/qd/../i/QD/lib/libqd"
     if [ $? = 0 ]; then
         ORG=`otool -L $_file  | grep QD | awk '{print $1}'` ;\
         _FIX=`otool -L $_file | grep QD | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
