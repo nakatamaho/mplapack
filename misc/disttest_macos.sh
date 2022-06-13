14f66537999fc51ff265953ae02c79e1ef3d3f70
Nakata Maho <maho.nakata@gmail.com>
2022-06-13 13:50:26 +0900

    .

--- misc/disttest_macos.sh
+++ misc/disttest_macos.sh
@@ -5,7 +5,7 @@ cd $TOPDIR
 pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
 aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
-./configure
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes
 make dist-xz
 ####################
 rm -rf $TOPDIR/tmp
