d81033f867f793886b1a3cd3bb5020d5f40dbc5f
Nakata Maho <maho.nakata@gmail.com>
2022-08-09 12:21:24 +0900

    .

--- misc/dockertest_amd64.sh
+++ misc/dockertest_amd64.sh
@@ -1,4 +1,4 @@
-DOCKERFILES=`ls Dockerfile* | grep -v fable`
+DOCKERFILES=`ls Dockerfile* | grep -v AArch64`
 for _dockerfile in $DOCKERFILES; do
     /usr/bin/time docker build -t mplapack:${_dockerfile} -f ${_dockerfile} . 2>&1 | tee log.${_dockerfile}
 done
