78fdc6d3dfa7e4eef038f21deca0463b0660feb0
NAKATA Maho <maho.nakata@gmail.com>
2022-08-12 05:44:52 +0900

    a small fix

--- benchmark/mplapack/go.Rgetrf_cuda.sh
+++ benchmark/mplapack/go.Rgetrf_cuda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-Rgetrf.dd_cuda_total  -NOCHECK -TOTALSTEPS 700 -STEPK 5 -STEPM 5 -STEPN 5 -LOOP 3   >& log.Rgetrf.dd_cuda_total
+./Rgetrf.dd_cuda_total  -NOCHECK -TOTALSTEPS 700 -STEPK 5 -STEPM 5 -STEPN 5 -LOOP 3   >& log.Rgetrf.dd_cuda_total
 
 gnuplot Rgetrf_cuda.plt > Rgetrf_cuda.eps
--- benchmark/mplapack/go.Rpotrf_cuda.sh
+++ benchmark/mplapack/go.Rpotrf_cuda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-Rpotrf.dd_cuda_total  -NOCHECK -TOTALSTEPS 700 -STEPK 5 -STEPM 5 -STEPN 5 -LOOP 3   >& log.Rpotrf.dd_cuda_total
+./Rpotrf.dd_cuda_total  -NOCHECK -TOTALSTEPS 700 -STEPK 5 -STEPM 5 -STEPN 5 -LOOP 3   >& log.Rpotrf.dd_cuda_total
 
 gnuplot Rpotrf_cuda.plt > Rpotrf_cuda.eps
