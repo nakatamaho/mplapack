79ed630d5045ba776bf4d5b1349c531c392e3961
NAKATA Maho <maho.nakata@gmail.com>
2022-08-12 15:29:11 +0900

    Add some benchmark results

---
+++ benchmark/results/getresults.sh
@@ -0,0 +1,14 @@
+cd /home/docker/mplapack/benchmark/results
+
+mkdir -p 2022/x86_64-apple-darwin20.6.0-Corei5-8500B
+cd 2022/x86_64-apple-darwin20.6.0-Corei5-8500B
+rsync -arv maho@172.27.109.87:~/MPLAPACK/lib/*/mplapack/benchmark/log.* .
+rsync -arv maho@172.27.109.87:~/MPLAPACK/lib/*/mplapack/benchmark/*.plt .
+rsync -arv maho@172.27.109.87:~/MPLAPACK/lib/*/mplapack/benchmark/*.eps .
+ 
+mkdir -p 2022/x86_64-pc-linux-gnu-Xeon-E5-2623
+cd 2022/x86_64-pc-linux-gnu-Xeon-E5-2623
+rsync -arv maho@dadeba:~/MPLAPACK/lib/*/mplapack/benchmark/log.* .
+rsync -arv maho@dadeba:~/MPLAPACK/lib/*/mplapack/benchmark/*.plt .
+rsync -arv maho@dadeba:~/MPLAPACK/lib/*/mplapack/benchmark/*.eps .
+
