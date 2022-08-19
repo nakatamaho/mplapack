772a865f1ac9421af489fa6e1d1aaa4eae4c1989
NAKATA Maho <maho.nakata@gmail.com>
2022-08-19 20:23:04 +0900

    .

--- benchmark/results/getresults.sh
+++ benchmark/results/getresults.sh
@@ -22,3 +22,10 @@ rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/*.pdf .
 rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/*.plt logs/
 rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/log.* logs/
 
+cd /home/docker/mplapack/benchmark/results
+mkdir -p 2022/aarch64-unknown-linux-gnu-raspberry-pi4
+mkdir -p 2022/aarch64-unknown-linux-gnu-raspberry-pi4/logs
+cd 2022/aarch64-unknown-linux-gnu-raspberry-pi4/logs
+rsync -arv maho@172.27.109.65:/data/mnt/MPLAPACK/lib/aarch64-unknown-linux-gnu/mplapack/benchmark/*.pdf .
+rsync -arv maho@172.27.109.65:/data/mnt/MPLAPACK/lib/aarch64-unknown-linux-gnu/mplapack/benchmark/*.plt logs/
+rsync -arv maho@172.27.109.65:/data/mnt/MPLAPACK/lib/aarch64-unknown-linux-gnu/mplapack/benchmark/log.* logs/
