4d40a517e1303a0705177e798cc4d153c577fa41
NAKATA Maho <maho.nakata@gmail.com>
2022-08-24 14:40:47 +0900

    .

--- benchmark/results/getresults.sh
+++ benchmark/results/getresults.sh
@@ -14,13 +14,21 @@ rsync -arv maho@dadeba:~/MPLAPACK/lib/*/mplapack/benchmark/*.pdf .
 rsync -arv maho@dadeba:~/MPLAPACK/lib/*/mplapack/benchmark/*.plt logs/
 rsync -arv maho@dadeba:~/MPLAPACK/lib/*/mplapack/benchmark/log.* logs/
 
+#cd /home/docker/mplapack/benchmark/results
+#mkdir -p 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-RTX3080
+#mkdir -p 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-RTX3080/logs
+#cd 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-RTX3080
+#rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/*.pdf .
+#rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/*.plt logs/
+#rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/log.* logs/
+
 cd /home/docker/mplapack/benchmark/results
-mkdir -p 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-RTX3080
-mkdir -p 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-RTX3080/logs
-cd 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-RTX3080
-rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/*.pdf .
-rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/*.plt logs/
-rsync -arv ~/MPLAPACK/lib/*/mplapack/benchmark/log.* logs/
+mkdir -p 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-inteloneapi
+mkdir -p 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-inteloneapi/logs
+cd 2022/x86_64-pc-linux-gnu-Ryzen-Threadripper-3970X-inteloneapi
+rsync -arv ~/MPLAPACK_INTELONEAPI/lib/*/mplapack/benchmark/*.pdf .
+rsync -arv ~/MPLAPACK_INTELONEAPI/lib/*/mplapack/benchmark/*.plt logs/
+rsync -arv ~/MPLAPACK_INTELONEAPI/lib/*/mplapack/benchmark/log.* logs/
 
 cd /home/docker/mplapack/benchmark/results
 mkdir -p 2022/aarch64-unknown-linux-gnu-raspberry-pi4
