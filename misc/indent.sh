cd61dcc54980b2381b4dc4fdd9a36c20c232d2d2
NAKATA Maho <maho.nakata@gmail.com>
2022-09-05 10:45:03 +0900

    .

--- misc/indent.sh
+++ misc/indent.sh
@@ -1,4 +1,4 @@
-FILES=`ls *.cpp *.h *h.in`
+FILES=`ls *.cpp *.h *h.in *.hpp`
 
 for _file in $FILES; do
 clang-format -style="{BasedOnStyle: llvm, IndentWidth: 4, ColumnLimit: 10000, SortIncludes: false}" $_file > ${_file}__ ; mv ${_file}__ ${_file}
