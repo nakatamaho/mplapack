f1710bc0d8e12d948f4964c64393bbda2f6ea052
NAKATA Maho <maho.nakata@gmail.com>
2023-12-01 10:57:57 +0900

    .

--- misc/conv_all_blas.sh
+++ misc/conv_all_blas.sh
@@ -1,22 +1,28 @@
 cd ~/mplapack/mpblas/reference
-FILES=`ls ~/mplapack/external/lapack/work/internal/lapack-3.9.1/BLAS/SRC/d*.f ~/mplapack/external/lapack/work/internal/lapack-3.9.1/BLAS/SRC/z*.f ~/mplapack/external/lapack/work/internal/lapack-3.9.1/BLAS/SRC/id*.f ~/mplapack/external/lapack/work/internal/lapack-3.9.1/BLAS/SRC/iz*.f | grep -v dsdot`
+LAPACKVERSION=3.12.0
 
-rm -f BLAS_LIST BLAS_LIST_  BLAS_LIST__
-echo "sed \\" > BLAS_LIST
-echo "-e 's///g'" >> BLAS_LIST__
+# dsdot and sdsdot are mixed precsion version
+FILES=`ls ~/mplapack/external/lapack/work/internal/lapack-$LAPACKVERSION/BLAS/SRC/d*.f*  \
+          ~/mplapack/external/lapack/work/internal/lapack-$LAPACKVERSION/BLAS/SRC/z*.f*  \
+          ~/mplapack/external/lapack/work/internal/lapack-$LAPACKVERSION/BLAS/SRC/id*.f* \ 
+          ~/mplapack/external/lapack/work/internal/lapack-$LAPACKVERSION/BLAS/SRC/iz*.f* \
+           | grep -v dsdot -v sdsdot`
+
+# filename (subroutine) name conversion
+# dgemm.f -> DGEMM -> Rgemm
+# dnrm2.f90 -> DNRM2 -> Rnrm2
+# etc.
+
+rm BLAS_TO_MPBLAS_FUNCTIONLIST.txt
+echo "sed \\"      > BLAS_TO_MPBLAS_FUNCTIONLIST.txt
 
 for _file in $FILES; do
-oldfilename=`basename $_file | sed -e 's/\.f$//'` 
-oldfilenameUP=`basename $_file | sed -e 's/\.f$//' | tr a-z A-Z`
-newfilename=`basename $_file | sed -e 's/^zdscal/CRscal/g' -e 's/^zdrot/CRrot/g' -e 's/^dcabs/RCabs/g' -e 's/^dzasum/RCasum/g' -e 's/^dznrm2/RCnrm2/g' | sed -e 's/^d/R/' -e 's/^z/C/' -e 's/^id/iR/' -e 's/^iz/iC/' -e 's/\.f$//'`
-echo "-e 's/$oldfilename/$newfilename/g' \\" >> BLAS_LIST_
-echo "-e 's/$oldfilenameUP/$newfilename/g' \\" >> BLAS_LIST_
+    oldfilename=`basename $_file | sed -e 's/\.f$//' | sed -e 's/\.f90$//' ` 
+    newfilename=`basename $_file | sed -e 's/^zdscal/CRscal/g' -e 's/^zdrot/CRrot/g' -e 's/^dcabs/RCabs/g' -e 's/^dzasum/RCasum/g' -e 's/^dznrm2/RCnrm2/g' | sed -e 's/^d/R/' -e 's/^z/C/' -e 's/^id/iR/' -e 's/^iz/iC/' -e 's/\.f$//' -e 's/\.f90$//' `
+     echo "-e 's/$oldfilename/$newfilename/g' \\"   >> BLAS_TO_MPBLAS_FUNCTIONLIST.txt
 done
-cat BLAS_LIST_ | sort -r > BLAS_LIST___
-mv BLAS_LIST___  BLAS_LIST_
-cat BLAS_LIST BLAS_LIST_  BLAS_LIST__ > ll
-mv ll BLAS_LIST
-rm BLAS_LIST_*
+sed -i '$ s/\\$//' BLAS_TO_MPBLAS_FUNCTIONLIST.txt
+exit
 
 for _file in $FILES; do
 bash ~/mplapack/misc/fable_convert_blas.sh $_file
