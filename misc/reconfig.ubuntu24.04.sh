7e3faf778b7a26d7a80a1a241e2616aeb113e6e2
NAKATA Maho <maho.nakata@gmail.com>
2025-11-16 09:41:23 +0900

    .

---
+++ misc/reconfig.ubuntu24.04.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USE_CCACHE=yes
+
+if [ x"$USE_CCACHE" = x"yes" ] ; then
+CXX="ccache g++" ; export CXX
+CC="ccache gcc" ; export CC
+FC="gfortran"; export FC
+ccache -M 80G
+else
+CXX="g++" ; export CXX
+CC="gcc" ; export CC
+FC="gfortran"; export FC
+fi
+
+pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
+
+aclocal ; autoconf ; automake --add-missing
+autoreconf --force --install
+if [ `uname -m` = "x86_64" ]; then
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes --enable-benchmark=yes
+else
+./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-test=yes --enable-benchmark=yes
+fi
