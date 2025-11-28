751b2ca91476ea5f07b542524c4e2191bd490c26
NAKATA Maho <maho.nakata@gmail.com>
2025-11-28 11:47:14 +0900

    .

--- mpblas/reference/Rrotm.cpp
+++ mpblas/reference/Rrotm.cpp
@@ -29,9 +29,9 @@
 #include <mpblas.h>
 
 void Rrotm(INTEGER const n, REAL *dx, INTEGER const incx, REAL *dy, INTEGER const incy, REAL *dparam) {
+    //
     REAL two = 2.0;
     REAL zero = 0.0;
-    //
     //
     REAL dflag = dparam[0];
     if (n <= 0 || (dflag + two == zero)) {
