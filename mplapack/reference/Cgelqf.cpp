d742ba1598f4abb06df67a0da400f118a7b632d6
NAKATA Maho <maho.nakata@gmail.com>
2025-12-01 14:34:44 +0900

    .

--- mplapack/reference/Cgelqf.cpp
+++ mplapack/reference/Cgelqf.cpp
@@ -31,10 +31,12 @@
 
 void Cgelqf(INTEGER const m, INTEGER const n, COMPLEX *a, INTEGER const lda, COMPLEX *tau, COMPLEX *work, INTEGER const lwork, INTEGER &info) {
     //
+    // Test the input arguments
+    //
     info = 0;
     INTEGER nb = iMlaenv(1, "Cgelqf", " ", m, n, -1, -1);
     INTEGER lwkopt = m * nb;
-    work[1 - 1] = lwkopt;
+    work[0] = lwkopt;
     bool lquery = (lwork == -1);
     if (m < 0) {
         info = -1;
@@ -56,7 +58,7 @@ void Cgelqf(INTEGER const m, INTEGER const n, COMPLEX *a, INTEGER const lda, COM
     //
     INTEGER k = min(m, n);
     if (k == 0) {
-        work[1 - 1] = 1;
+        work[0] = 1;
         return;
     }
     //
@@ -122,7 +124,7 @@ void Cgelqf(INTEGER const m, INTEGER const n, COMPLEX *a, INTEGER const lda, COM
         Cgelq2(m - i + 1, n - i + 1, &a[(i - 1) + (i - 1) * lda], lda, &tau[i - 1], work, iinfo);
     }
     //
-    work[1 - 1] = iws;
+    work[0] = iws;
     //
     // End of Cgelqf
     //
