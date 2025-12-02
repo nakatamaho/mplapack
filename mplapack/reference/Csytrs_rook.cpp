ceadd966ca781764ea3f5a30c09c0567bb1ca40b
NAKATA Maho <maho.nakata@gmail.com>
2025-12-02 12:14:02 +0900

    .

--- mplapack/reference/Csytrs_rook.cpp
+++ mplapack/reference/Csytrs_rook.cpp
@@ -42,10 +42,6 @@ void Csytrs_rook(const char *uplo, INTEGER const n, INTEGER const nrhs, COMPLEX 
     COMPLEX bkm1 = 0.0;
     COMPLEX bk = 0.0;
     //
-    //
-    //
-    //
-    //
     info = 0;
     upper = Mlsame(uplo, "U");
     if (!upper && !Mlsame(uplo, "L")) {
