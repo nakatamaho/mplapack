89f441ee7ca87e3ed36ec3f80bb7bc899b2bcf90
NAKATA Maho <maho.nakata@gmail.com>
2025-12-02 12:24:32 +0900

    .

--- mplapack/reference/Rlauum.cpp
+++ mplapack/reference/Rlauum.cpp
@@ -31,10 +31,6 @@
 
 void Rlauum(const char *uplo, INTEGER const n, REAL *a, INTEGER const lda, INTEGER &info) {
     //
-    //
-    //
-    //
-    //
     // Test the input parameters.
     //
     info = 0;
