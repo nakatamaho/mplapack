eb2ab5c0770e80208fd37cf27142d03143453b36
NAKATA Maho <maho.nakata@gmail.com>
2025-12-03 18:31:51 +0900

    .

--- mplapack/reference/Rormr3.cpp
+++ mplapack/reference/Rormr3.cpp
@@ -31,10 +31,6 @@
 
 void Rormr3(const char *side, const char *trans, INTEGER const m, INTEGER const n, INTEGER const k, INTEGER const l, REAL *a, INTEGER const lda, REAL *tau, REAL *c, INTEGER const ldc, REAL *work, INTEGER &info) {
     //
-    //
-    //
-    //
-    //
     // Test the input arguments
     //
     info = 0;
