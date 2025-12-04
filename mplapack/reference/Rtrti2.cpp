7a53b4c6febe1c36e44a4f88ca0075f7ba198c25
NAKATA Maho <maho.nakata@gmail.com>
2025-12-04 14:33:24 +0900

    .

--- mplapack/reference/Rtrti2.cpp
+++ mplapack/reference/Rtrti2.cpp
@@ -31,10 +31,6 @@
 
 void Rtrti2(const char *uplo, const char *diag, INTEGER const n, REAL *a, INTEGER const lda, INTEGER &info) {
     //
-    //
-    //
-    //
-    //
     // Test the input parameters.
     //
     info = 0;
