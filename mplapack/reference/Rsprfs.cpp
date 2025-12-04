82204bf7f9254a83b22edb2cd345244fe31acbcd
NAKATA Maho <maho.nakata@gmail.com>
2025-12-04 14:35:40 +0900

    .

--- mplapack/reference/Rsprfs.cpp
+++ mplapack/reference/Rsprfs.cpp
@@ -39,7 +39,7 @@ void Rsprfs(const char *uplo, INTEGER const n, INTEGER const nrhs, REAL *ap, REA
     REAL safe1 = 0.0;
     REAL safe2 = 0.0;
     INTEGER count = 0;
-    const REAL three = 3.0e+0;
+    const REAL three = 3.0;
     REAL lstres = 0.0;
     const REAL one = 1.0;
     INTEGER i = 0;
@@ -48,15 +48,10 @@ void Rsprfs(const char *uplo, INTEGER const n, INTEGER const nrhs, REAL *ap, REA
     REAL s = 0.0;
     REAL xk = 0.0;
     INTEGER ik = 0;
-    const REAL two = 2.0e+0;
+    const REAL two = 2.0;
     const INTEGER itmax = 5;
     INTEGER kase = 0;
     INTEGER isave[3];
-    //
-    //
-    //
-    //
-    // .. Local Arrays ..
     //
     // Test the input parameters.
     //
@@ -158,9 +153,9 @@ void Rsprfs(const char *uplo, INTEGER const n, INTEGER const nrhs, REAL *ap, REA
         s = zero;
         for (i = 1; i <= n; i = i + 1) {
             if (work[i - 1] > safe2) {
-                s = max(s, REAL(abs(work[(n + i) - 1]) / work[i - 1]));
+                s = max(s, abs(work[(n + i) - 1]) / work[i - 1]);
             } else {
-                s = max(s, REAL((abs(work[(n + i) - 1]) + safe1) / (work[i - 1] + safe1)));
+                s = max(s, (abs(work[(n + i) - 1]) + safe1) / (work[i - 1] + safe1));
             }
         }
         berr[j - 1] = s;
@@ -240,7 +235,7 @@ void Rsprfs(const char *uplo, INTEGER const n, INTEGER const nrhs, REAL *ap, REA
         //
         lstres = zero;
         for (i = 1; i <= n; i = i + 1) {
-            lstres = max(lstres, REAL(abs(x[(i - 1) + (j - 1) * ldx])));
+            lstres = max(lstres, abs(x[(i - 1) + (j - 1) * ldx]));
         }
         if (lstres != zero) {
             ferr[j - 1] = ferr[j - 1] / lstres;
@@ -248,6 +243,6 @@ void Rsprfs(const char *uplo, INTEGER const n, INTEGER const nrhs, REAL *ap, REA
         //
     }
     //
-    // End of RsprFS
+    // End of Rsprfs
     //
 }
