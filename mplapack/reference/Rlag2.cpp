ae89ad109108c9a33a67a8eef85da9bd1d7b20b5
NAKATA Maho <maho.nakata@gmail.com>
2025-12-04 12:25:45 +0900

    .

--- mplapack/reference/Rlag2.cpp
+++ mplapack/reference/Rlag2.cpp
@@ -29,11 +29,7 @@
 #include <mpblas.h>
 #include <mplapack.h>
 
-void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const safmin, REAL &scale1, REAL &scale2, REAL &wr1, REAL &wr2, REAL &wi) {
-    //
-    //
-    //
-    //
+void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const &safmin, REAL &scale1, REAL &scale2, REAL &wr1, REAL &wr2, REAL &wi) {
     //
     REAL rtmin = sqrt(safmin);
     const REAL one = 1.0;
@@ -42,7 +38,7 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     //
     // Scale A
     //
-    REAL anorm = max({REAL(abs(a[0]) + abs(a[(2 - 1)])), REAL(abs(a[(2 - 1) * lda]) + abs(a[(2 - 1) + (2 - 1) * lda])), safmin});
+    REAL anorm = max(abs(a[0]) + abs(a[(2 - 1) + (1 - 1) * lda]), abs(a[(2 - 1) * lda]) + abs(a[(2 - 1) + (2 - 1) * lda]), safmin);
     REAL ascale = one / anorm;
     REAL a11 = ascale * a[0];
     REAL a21 = ascale * a[(2 - 1)];
@@ -54,7 +50,7 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     REAL b11 = b[0];
     REAL b12 = b[(2 - 1) * ldb];
     REAL b22 = b[(2 - 1) + (2 - 1) * ldb];
-    REAL bmin = rtmin * max({REAL(abs(b11)), REAL(abs(b12)), REAL(abs(b22)), rtmin});
+    REAL bmin = rtmin * max(abs(b11), abs(b12), abs(b22), rtmin);
     if (abs(b11) < bmin) {
         b11 = sign(bmin, b11);
     }
@@ -64,8 +60,8 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     //
     // Scale B
     //
-    REAL bnorm = max({REAL(abs(b11)), REAL(abs(b12) + abs(b22)), safmin});
-    REAL bsize = max(REAL(abs(b11)), REAL(abs(b22)));
+    REAL bnorm = max(abs(b11), abs(b12) + abs(b22), safmin);
+    REAL bsize = max(abs(b11), abs(b22));
     REAL bscale = one / bsize;
     b11 = b11 * bscale;
     b12 = b12 * bscale;
@@ -83,7 +79,7 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     REAL as22 = 0.0;
     REAL ss = 0.0;
     REAL abi22 = 0.0;
-    const REAL two = 2.0e+0;
+    const REAL two = 2.0;
     const REAL half = one / two;
     REAL pp = 0.0;
     REAL shift = 0.0;
@@ -139,7 +135,7 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
         // Compute smaller eigenvalue
         //
         wsmall = shift + diff;
-        if (half * abs(wbig) > max(REAL(abs(wsmall)), safmin)) {
+        if (half * abs(wbig) > max(abs(wsmall), safmin)) {
             wdet = (a11 * a22 - a12 * a21) * (binv11 * binv22);
             wsmall = wdet / wbig;
         }
@@ -181,13 +177,13 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     REAL c3 = bsize * safmin;
     REAL c4 = 0.0;
     if (ascale <= one && bsize <= one) {
-        c4 = min(one, REAL((ascale / safmin) * bsize));
+        c4 = min(one, (ascale / safmin) * bsize);
     } else {
         c4 = one;
     }
     REAL c5 = 0.0;
     if (ascale <= one || bsize <= one) {
-        c5 = min(one, REAL(ascale * bsize));
+        c5 = min(one, ascale * bsize);
     } else {
         c5 = one;
     }
@@ -195,8 +191,8 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     // Scale first eigenvalue
     //
     REAL wabs = abs(wr1) + abs(wi);
-    const REAL fuzzy1 = one + 1.0e-5;
-    REAL wsize = max({safmin, c1, REAL(fuzzy1 * (wabs * c2 + c3)), min(c4, REAL(half * max(wabs, c5)))});
+    const REAL fuzzy1 = one + 1e-05;
+    REAL wsize = max(safmin, c1, fuzzy1 * (wabs * c2 + c3), min(c4, half * max(wabs, c5)));
     REAL wscale = 0.0;
     if (wsize != one) {
         wscale = one / wsize;
@@ -219,7 +215,7 @@ void Rlag2(REAL *a, INTEGER const lda, REAL *b, INTEGER const ldb, REAL const sa
     // Scale second eigenvalue (if real)
     //
     if (wi == zero) {
-        wsize = max({safmin, c1, REAL(fuzzy1 * (abs(wr2) * c2 + c3)), min(c4, REAL(half * max(REAL(abs(wr2)), c5)))});
+        wsize = max(safmin, c1, fuzzy1 * (abs(wr2) * c2 + c3), min(c4, half * max(abs(wr2), c5)));
         if (wsize != one) {
             wscale = one / wsize;
             if (wsize > one) {
