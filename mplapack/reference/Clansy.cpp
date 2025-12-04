77e3f624f0e051c9cd1117f57c2d375ba87f1f5f
NAKATA Maho <maho.nakata@gmail.com>
2025-12-04 18:13:44 +0900

    .

--- mplapack/reference/Clansy.cpp
+++ mplapack/reference/Clansy.cpp
@@ -32,11 +32,6 @@
 REAL Clansy(const char *norm, const char *uplo, INTEGER const n, COMPLEX *a, INTEGER const lda, REAL *work) {
     REAL return_value = 0.0;
     //
-    //
-    //
-    //
-    // .. Local Arrays ..
-    //
     const REAL zero = 0.0;
     REAL value = 0.0;
     INTEGER j = 0;
@@ -72,7 +67,7 @@ REAL Clansy(const char *norm, const char *uplo, INTEGER const n, COMPLEX *a, INT
                 }
             }
         }
-    } else if ((Mlsame(norm, "I")) || (Mlsame(norm, "O")) || ((Mlsame(norm, "1")))) {
+    } else if ((Mlsame(norm, "I")) || (Mlsame(norm, "O")) || (Mlsame(norm, "1"))) {
         //
         // Find normI(A) ( = norm1(A), since A is symmetric).
         //
@@ -116,35 +111,35 @@ REAL Clansy(const char *norm, const char *uplo, INTEGER const n, COMPLEX *a, INT
         // SSQ(2) is sum-of-squares
         // For better accuracy, sum each column separately.
         //
-        ssq[1 - 1] = zero;
-        ssq[2 - 1] = one;
+        ssq[0] = zero;
+        ssq[1] = one;
         //
         // Sum off-diagonals
         //
         if (Mlsame(uplo, "U")) {
             for (j = 2; j <= n; j = j + 1) {
-                colssq[1 - 1] = zero;
-                colssq[2 - 1] = one;
-                Classq(j - 1, &a[(j - 1) * lda], 1, colssq[1 - 1], colssq[2 - 1]);
+                colssq[0] = zero;
+                colssq[1] = one;
+                Classq(j - 1, &a[(j - 1) * lda], 1, colssq[0], colssq[1]);
                 Rcombssq(ssq, colssq);
             }
         } else {
             for (j = 1; j <= n - 1; j = j + 1) {
-                colssq[1 - 1] = zero;
-                colssq[2 - 1] = one;
-                Classq(n - j, &a[((j + 1) - 1) + (j - 1) * lda], 1, colssq[1 - 1], colssq[2 - 1]);
+                colssq[0] = zero;
+                colssq[1] = one;
+                Classq(n - j, &a[((j + 1) - 1) + (j - 1) * lda], 1, colssq[0], colssq[1]);
                 Rcombssq(ssq, colssq);
             }
         }
-        ssq[2 - 1] = 2 * ssq[2 - 1];
+        ssq[1] = 2 * ssq[1];
         //
         // Sum diagonal
         //
-        colssq[1 - 1] = zero;
-        colssq[2 - 1] = one;
-        Classq(n, a, lda + 1, colssq[1 - 1], colssq[2 - 1]);
+        colssq[0] = zero;
+        colssq[1] = one;
+        Classq(n, a, lda + 1, colssq[0], colssq[1]);
         Rcombssq(ssq, colssq);
-        value = ssq[1 - 1] * sqrt(ssq[2 - 1]);
+        value = ssq[0] * sqrt(ssq[1]);
     }
     //
     return_value = value;
