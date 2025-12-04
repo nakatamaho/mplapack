314e9dba3013699d96999d6922fea5854127b8ac
NAKATA Maho <maho.nakata@gmail.com>
2025-12-04 13:08:04 +0900

    .

--- mplapack/reference/Rpbrfs.cpp
+++ mplapack/reference/Rpbrfs.cpp
@@ -39,7 +39,7 @@ void Rpbrfs(const char *uplo, INTEGER const n, INTEGER const kd, INTEGER const n
     REAL safe1 = 0.0;
     REAL safe2 = 0.0;
     INTEGER count = 0;
-    const REAL three = 3.0e+0;
+    const REAL three = 3.0;
     REAL lstres = 0.0;
     const REAL one = 1.0;
     INTEGER i = 0;
@@ -47,15 +47,10 @@ void Rpbrfs(const char *uplo, INTEGER const n, INTEGER const kd, INTEGER const n
     REAL s = 0.0;
     REAL xk = 0.0;
     INTEGER l = 0;
-    const REAL two = 2.0e+0;
+    const REAL two = 2.0;
     const INTEGER itmax = 5;
     INTEGER kase = 0;
     INTEGER isave[3];
-    //
-    //
-    //
-    //
-    // .. Local Arrays ..
     //
     // Test the input parameters.
     //
@@ -158,9 +153,9 @@ void Rpbrfs(const char *uplo, INTEGER const n, INTEGER const kd, INTEGER const n
         s = zero;
         for (i = 1; i <= n; i = i + 1) {
             if (work[i - 1] > safe2) {
-                s = max(s, REAL(abs(work[(n + i) - 1]) / work[i - 1]));
+                s = max(s, abs(work[(n + i) - 1]) / work[i - 1]);
             } else {
-                s = max(s, REAL((abs(work[(n + i) - 1]) + safe1) / (work[i - 1] + safe1)));
+                s = max(s, (abs(work[(n + i) - 1]) + safe1) / (work[i - 1] + safe1));
             }
         }
         berr[j - 1] = s;
@@ -240,7 +235,7 @@ void Rpbrfs(const char *uplo, INTEGER const n, INTEGER const kd, INTEGER const n
         //
         lstres = zero;
         for (i = 1; i <= n; i = i + 1) {
-            lstres = max(lstres, REAL(abs(x[(i - 1) + (j - 1) * ldx])));
+            lstres = max(lstres, abs(x[(i - 1) + (j - 1) * ldx]));
         }
         if (lstres != zero) {
             ferr[j - 1] = ferr[j - 1] / lstres;
