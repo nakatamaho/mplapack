10b722024531b4e7ca9261605fd280fe9ea91ced
NAKATA Maho <maho.nakata@gmail.com>
2025-12-05 12:12:20 +0900

    .

--- mplapack/reference/Cupmtr.cpp
+++ mplapack/reference/Cupmtr.cpp
@@ -31,10 +31,6 @@
 
 void Cupmtr(const char *side, const char *uplo, const char *trans, INTEGER const m, INTEGER const n, COMPLEX *ap, COMPLEX *tau, COMPLEX *c, INTEGER const ldc, COMPLEX *work, INTEGER &info) {
     //
-    //
-    //
-    //
-    //
     // Test the input arguments
     //
     info = 0;
@@ -111,7 +107,7 @@ void Cupmtr(const char *side, const char *uplo, const char *trans, INTEGER const
             mi = m;
         }
         //
-        for (i = i1; i3 >= 0 ? i <= i2 : i >= i2; i = i + i3) {
+        for (i = i1; i3 > 0 ? i <= i2 : i >= i2; i = i + i3) {
             if (left) {
                 //
                 // H(i) or H(i)**H is applied to C(1:i,1:n)
@@ -168,7 +164,7 @@ void Cupmtr(const char *side, const char *uplo, const char *trans, INTEGER const
             ic = 1;
         }
         //
-        for (i = i1; i3 >= 0 ? i <= i2 : i >= i2; i = i + i3) {
+        for (i = i1; i3 > 0 ? i <= i2 : i >= i2; i = i + i3) {
             aii = ap[ii - 1];
             ap[ii - 1] = one;
             if (left) {
