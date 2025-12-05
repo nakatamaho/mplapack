05425fa6e742c8ad5ff3e063dd2707c46cfbd921
NAKATA Maho <maho.nakata@gmail.com>
2025-12-05 18:52:11 +0900

    .

--- mplapack/reference/Cgeevx.cpp
+++ mplapack/reference/Cgeevx.cpp
@@ -29,7 +29,7 @@
 #include <mpblas.h>
 #include <mplapack.h>
 
-void Cgeevx(const char *balanc, const char *jobvl, const char *jobvr, const char *sense, INTEGER const n, COMPLEX *a, INTEGER const lda, COMPLEX *w, COMPLEX *vl, INTEGER const ldvl, COMPLEX *vr, INTEGER const ldvr, INTEGER ilo, INTEGER ihi, REAL *scale, REAL &abnrm, REAL *rconde, REAL *rcondv, COMPLEX *work, INTEGER const lwork, REAL *rwork, INTEGER &info) {
+void Cgeevx(const char *balanc, const char *jobvl, const char *jobvr, const char *sense, INTEGER const n, COMPLEX *a, INTEGER const lda, COMPLEX *w, COMPLEX *vl, INTEGER const ldvl, COMPLEX *vr, INTEGER const ldvr, INTEGER const ilo, INTEGER const ihi, REAL *scale, REAL &abnrm, REAL *rconde, REAL *rcondv, COMPLEX *work, INTEGER const lwork, REAL *rwork, INTEGER &info) {
     bool lquery = false;
     bool wantvl = false;
     bool wantvr = false;
@@ -39,7 +39,7 @@ void Cgeevx(const char *balanc, const char *jobvl, const char *jobvr, const char
     bool wntsnb = false;
     INTEGER minwrk = 0;
     INTEGER maxwrk = 0;
-    bool select[4];
+    bool select[1];
     INTEGER nout = 0;
     INTEGER ierr = 0;
     INTEGER lwork_trevc = 0;
@@ -62,12 +62,6 @@ void Cgeevx(const char *balanc, const char *jobvl, const char *jobvr, const char
     REAL scl = 0.0;
     INTEGER k = 0;
     COMPLEX tmp = 0.0;
-    //
-    // -- LAPACK driver routine --
-    //
-    //
-    //
-    // .. Local Arrays ..
     //
     // Test the input arguments
     //
@@ -182,6 +176,7 @@ void Cgeevx(const char *balanc, const char *jobvl, const char *jobvr, const char
     eps = Rlamch("P");
     smlnum = Rlamch("S");
     bignum = one / smlnum;
+    Rlabad(smlnum, bignum);
     smlnum = sqrt(smlnum) / eps;
     bignum = one / smlnum;
     //
