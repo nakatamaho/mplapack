4b91c0c9d4905408b3cd27cd0fd63adfd96a4f78
NAKATA Maho <maho.nakata@gmail.com>
2025-12-07 18:41:28 +0900

    .

--- mplapack/reference/Rlaed7.cpp
+++ mplapack/reference/Rlaed7.cpp
@@ -29,7 +29,7 @@
 #include <mpblas.h>
 #include <mplapack.h>
 
-void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER const tlvls, INTEGER const curlvl, INTEGER const curpbm, REAL *d, REAL *q, INTEGER const ldq, INTEGER *indxq, REAL rho, INTEGER const cutpnt, REAL *qstore, INTEGER *qptr, INTEGER *prmptr, INTEGER *perm, INTEGER *givptr, INTEGER *givcol, REAL *givnum, REAL *work, INTEGER *iwork, INTEGER &info) {
+void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER const tlvls, INTEGER const curlvl, INTEGER const curpbm, REAL *d, REAL *q, INTEGER const ldq, INTEGER *indxq, REAL const &rho, INTEGER const cutpnt, REAL *qstore, INTEGER *qptr, INTEGER *prmptr, INTEGER *perm, INTEGER *givptr, INTEGER *givcol, REAL *givnum, REAL *work, INTEGER *iwork, INTEGER &info) {
     INTEGER ldq2 = 0;
     INTEGER iz = 0;
     INTEGER idlmda = 0;
@@ -48,10 +48,6 @@ void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER c
     const REAL zero = 0.0;
     INTEGER n1 = 0;
     INTEGER n2 = 0;
-    //
-    //
-    //
-    //
     //
     // Test the input parameters.
     //
@@ -81,7 +77,7 @@ void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER c
     //
     // The following values are for bookkeeping purposes only.  They are
     // integer pointers which indicate the portion of the workspace
-    // used by a particular array in DLAED8 and DLAED9.
+    // used by a particular array in Rlaed8 and Rlaed9.
     //
     if (icompq == 1) {
         ldq2 = qsiz;
@@ -103,9 +99,9 @@ void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER c
     // Form the z-vector which consists of the last row of Q_1 and the
     // first row of Q_2.
     //
-    ptr = 1 + (INTEGER)pow((double)2, (double)tlvls);
+    ptr = 1 + pow(2, tlvls);
     for (i = 1; i <= curlvl - 1; i = i + 1) {
-        ptr += (INTEGER)pow((double)2, (double)(tlvls - i));
+        ptr += pow(2, (tlvls - i));
     }
     curr = ptr + curpbm;
     Rlaeda(n, tlvls, curlvl, curpbm, prmptr, perm, givptr, givcol, givnum, qstore, qptr, &work[iz - 1], &work[(iz + n) - 1], info);
@@ -122,7 +118,7 @@ void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER c
     //
     // Sort and Deflate eigenvalues.
     //
-    Rlaed8(icompq, k, n, qsiz, d, q, ldq, indxq, rho, cutpnt, &work[iz - 1], &work[idlmda - 1], &work[iq2 - 1], ldq2, &work[iw - 1], &perm[prmptr[curr - 1] - 1], givptr[(curr + 1) - 1], &givcol[(givptr[curr - 1] - 1) * 2], &givnum[(givptr[curr - 1] - 1) * 2], &iwork[indxp - 1], &iwork[indx - 1], info);
+    Rlaed8(icompq, k, n, qsiz, d, q, ldq, indxq, rho, cutpnt, &work[iz - 1], &work[idlmda - 1], &work[iq2 - 1], ldq2, &work[iw - 1], &perm[prmptr[curr - 1] - 1], givptr[(curr + 1) - 1], &givcol[(givptr[curr - 1] - 1) * ldgivcol], &givnum[(givptr[curr - 1] - 1) * ldgivnum], &iwork[indxp - 1], &iwork[indx - 1], info);
     prmptr[(curr + 1) - 1] = prmptr[curr - 1] + n;
     givptr[(curr + 1) - 1] += givptr[curr - 1];
     //
@@ -136,7 +132,7 @@ void Rlaed7(INTEGER const icompq, INTEGER const n, INTEGER const qsiz, INTEGER c
         if (icompq == 1) {
             Rgemm("N", "N", qsiz, k, k, one, &work[iq2 - 1], ldq2, &qstore[qptr[curr - 1] - 1], k, zero, q, ldq);
         }
-        qptr[(curr + 1) - 1] = qptr[curr - 1] + k * k;
+        qptr[(curr + 1) - 1] = qptr[curr - 1] + pow2(k);
         //
         // Prepare the INDXQ sorting permutation.
         //
