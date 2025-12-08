0cb058247ab97b63b5ee207cf0c6170b6d5061bf
NAKATA Maho <maho.nakata@gmail.com>
2025-12-08 17:03:34 +0900

    .

--- mplapack/reference/Rbbcsd.cpp
+++ mplapack/reference/Rbbcsd.cpp
@@ -119,7 +119,7 @@ void Rbbcsd(const char *jobu1, const char *jobu2, const char *jobv1t, const char
     //
     INTEGER i = 0;
     const REAL zero = 0.0;
-    const REAL piover2 = 1.570796326794897;
+    const REAL piover2 = pi(zero);
     for (i = 1; i <= q; i = i + 1) {
         if (theta[i - 1] < thresh) {
             theta[i - 1] = zero;
@@ -262,7 +262,7 @@ void Rbbcsd(const char *jobu1, const char *jobu2, const char *jobv1t, const char
                 }
             } else {
                 nu = sigma21;
-                mu = sqrt(1.0 - pow2(nu));
+                mu = sqrt(one - pow2(nu));
                 if (nu < thresh) {
                     mu = one;
                     nu = zero;
