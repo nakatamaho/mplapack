5822387c3e88f10fd3bf12228c47b805e0012b39
NAKATA Maho <maho.nakata@gmail.com>
2025-12-09 08:41:46 +0900

    .

--- mplapack/reference/Rggev3.cpp
+++ mplapack/reference/Rggev3.cpp
@@ -58,7 +58,7 @@ void Rggev3(const char *jobvl, const char *jobvr, INTEGER const n, REAL *a, INTE
     INTEGER icols = 0;
     INTEGER itau = 0;
     char chtemp;
-    bool ldumma;
+    bool ldumma[1];
     INTEGER in = 0;
     INTEGER jc = 0;
     REAL temp = 0.0;
@@ -138,7 +138,7 @@ void Rggev3(const char *jobvl, const char *jobvr, INTEGER const n, REAL *a, INTE
     }
     //
     if (info != 0) {
-        Mxerbla("Rggev3", -info);
+        Mxerbla("Rggev3 ", -info);
         return;
     } else if (lquery) {
         return;
@@ -155,6 +155,7 @@ void Rggev3(const char *jobvl, const char *jobvr, INTEGER const n, REAL *a, INTE
     eps = Rlamch("P");
     smlnum = Rlamch("S");
     bignum = one / smlnum;
+    Rlabad(smlnum, bignum);
     smlnum = sqrt(smlnum) / eps;
     bignum = one / smlnum;
     //
@@ -271,7 +272,7 @@ void Rggev3(const char *jobvl, const char *jobvr, INTEGER const n, REAL *a, INTE
         } else {
             chtemp = 'R';
         }
-        Rtgevc(&chtemp, "B", &ldumma, n, a, lda, b, ldb, vl, ldvl, vr, ldvr, n, in, &work[iwrk - 1], ierr);
+        Rtgevc(&chtemp, "B", ldumma, n, a, lda, b, ldb, vl, ldvl, vr, ldvr, n, in, &work[iwrk - 1], ierr);
         if (ierr != 0) {
             info = n + 2;
             goto statement_110;
@@ -288,11 +289,11 @@ void Rggev3(const char *jobvl, const char *jobvr, INTEGER const n, REAL *a, INTE
                 temp = zero;
                 if (alphai[jc - 1] == zero) {
                     for (jr = 1; jr <= n; jr = jr + 1) {
-                        temp = max(temp, REAL(abs(vl[(jr - 1) + (jc - 1) * ldvl])));
+                        temp = max(temp, abs(vl[(jr - 1) + (jc - 1) * ldvl]));
                     }
                 } else {
                     for (jr = 1; jr <= n; jr = jr + 1) {
-                        temp = max(temp, REAL(abs(vl[(jr - 1) + (jc - 1) * ldvl]) + abs(vl[(jr - 1) + ((jc + 1) - 1) * ldvl])));
+                        temp = max(temp, abs(vl[(jr - 1) + (jc - 1) * ldvl]) + abs(vl[(jr - 1) + ((jc + 1) - 1) * ldvl]));
                     }
                 }
                 if (temp < smlnum) {
@@ -321,11 +322,11 @@ void Rggev3(const char *jobvl, const char *jobvr, INTEGER const n, REAL *a, INTE
                 temp = zero;
                 if (alphai[jc - 1] == zero) {
                     for (jr = 1; jr <= n; jr = jr + 1) {
-                        temp = max(temp, REAL(abs(vr[(jr - 1) + (jc - 1) * ldvr])));
+                        temp = max(temp, abs(vr[(jr - 1) + (jc - 1) * ldvr]));
                     }
                 } else {
                     for (jr = 1; jr <= n; jr = jr + 1) {
-                        temp = max(temp, REAL(abs(vr[(jr - 1) + (jc - 1) * ldvr]) + abs(vr[(jr - 1) + ((jc + 1) - 1) * ldvr])));
+                        temp = max(temp, abs(vr[(jr - 1) + (jc - 1) * ldvr]) + abs(vr[(jr - 1) + ((jc + 1) - 1) * ldvr]));
                     }
                 }
                 if (temp < smlnum) {
