6cd8a581fbff8a438d242c07240a6aef029b5547
NAKATA Maho <maho.nakata@gmail.com>
2025-12-08 15:56:13 +0900

    .

--- fable/convert_lapack_all.sh
+++ fable/convert_lapack_all.sh
@@ -80,3 +80,5 @@ patch -p3 -R < ~/mplapack/fable/lapack-patches/patch-Cbbcsd.cpp
 patch -p3 -R < ~/mplapack/fable/lapack-patches/patch-Cpptrf.cpp
 patch -p3 -R < ~/mplapack/fable/lapack-patches/patch-Cpotf2.cpp
 patch -p3 -R < ~/mplapack/fable/lapack-patches/patch-Clarnv.cpp
+patch -p3 -R < ~/mplapack/fable/lapack-patches/patch-Ctrexc.cpp
+
