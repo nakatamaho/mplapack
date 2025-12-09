e4e3c9b6702aed6b8853c4fe03ad6b663d1bb3be
NAKATA Maho <maho.nakata@gmail.com>
2025-12-09 10:55:00 +0900

    .

--- mplapack/reference/Rlacon.cpp
+++ mplapack/reference/Rlacon.cpp
@@ -30,23 +30,18 @@
 #include <mplapack.h>
 
 void Rlacon(INTEGER const n, REAL *v, REAL *x, INTEGER *isgn, REAL &est, INTEGER &kase) {
-    REAL altsgn;
-    REAL estold;
-    INTEGER i;
-    INTEGER iter;
-    INTEGER j;
-    INTEGER jlast;
-    INTEGER jump;
-    REAL temp;
+    REAL altsgn = 0.0;
+    REAL estold = 0.0;
+    INTEGER i = 0;
+    INTEGER iter = 0;
+    INTEGER j = 0;
+    INTEGER jlast = 0;
+    INTEGER jump = 0;
+    REAL temp = 0.0;
     const REAL one = 1.0;
     const REAL zero = 0.0;
     const INTEGER itmax = 5;
-    const REAL two = 2.0e+0;
-    //
-    //
-    //
-    //
-    // .. Save statement ..
+    const REAL two = 2.0;
     //
     if (kase == 0) {
         for (i = 1; i <= n; i = i + 1) {
