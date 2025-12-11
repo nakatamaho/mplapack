d19dcc9d8088b251e7256ba3e469140b42736d04
NAKATA Maho <maho.nakata@gmail.com>
2025-12-11 09:58:39 +0900

    .

--- mplapack/reference/Claqr5.cpp
+++ mplapack/reference/Claqr5.cpp
@@ -30,28 +30,57 @@
 #include <mplapack.h>
 
 void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER const n, INTEGER const ktop, INTEGER const kbot, INTEGER const nshfts, COMPLEX *s, COMPLEX *h, INTEGER const ldh, INTEGER const iloz, INTEGER const ihiz, COMPLEX *z, INTEGER const ldz, COMPLEX *v, INTEGER const ldv, COMPLEX *u, INTEGER const ldu, INTEGER const nv, COMPLEX *wv, INTEGER const ldwv, INTEGER const nh, COMPLEX *wh, INTEGER const ldwh) {
-    //
     COMPLEX cdum = 0.0;
+    //
+    // ==== If there are no shifts, then there is nothing to do. ====
+    //
     if (nshfts < 2) {
         return;
     }
+    //
+    // ==== If the active block is empty or 1-by-1, then there
+    // .    is nothing to do. ====
+    //
     if (ktop >= kbot) {
         return;
     }
+    //
+    // ==== NSHFTS is supposed to be even, but if it is odd,
+    // .    then simply reduce it by one.  ====
+    //
     INTEGER ns = nshfts - mod(nshfts, 2);
+    //
+    // ==== Machine constants for deflation ====
+    //
     REAL safmin = Rlamch("SAFE MINIMUM");
     const REAL rone = 1.0;
     REAL safmax = rone / safmin;
     Rlabad(safmin, safmax);
     REAL ulp = Rlamch("PRECISION");
     REAL smlnum = safmin * (castREAL(n) / ulp);
+    //
+    // ==== Use accumulated reflections to update far-from-diagonal
+    // .    entries ? ====
+    //
     bool accum = (kacc22 == 1) || (kacc22 == 2);
+    //
+    // ==== clear trash ====
+    //
     const COMPLEX zero = COMPLEX(0.0, 0.0);
     if (ktop + 2 <= kbot) {
         h[((ktop + 2) - 1) + (ktop - 1) * ldh] = zero;
     }
+    //
+    // ==== NBMPS = number of 2-shift bulges in the chain ====
+    //
     INTEGER nbmps = ns / 2;
+    //
+    // ==== KDU = width of slab ====
+    //
     INTEGER kdu = 4 * nbmps;
+    //
+    // ==== Create and chase chains of NBMPS bulges ====
+    //
     INTEGER incol = 0;
     INTEGER jtop = 0;
     INTEGER ndcol = 0;
@@ -101,6 +130,8 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
         if (accum) {
             Claset("ALL", kdu, kdu, zero, one, u, ldu);
         }
+        //
+        // ==== Near-the-diagonal bulge chase.  The following loop
         // .    performs the near-the-diagonal part of a small bulge
         // .    multi-shift QR sweep.  Each 4*NBMPS column diagonal
         // .    chunk extends from column INCOL to column NDCOL
@@ -110,18 +141,29 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
         // .    may be less than KTOP and and NDCOL may be greater than
         // .    KBOT indicating phantom columns from which to chase
         // .    bulges before they are actually introduced or to which
+        // .    to chase bulges beyond column KBOT.)  ====
         //
         for (krcol = incol; krcol <= min(incol + 2 * nbmps - 1, kbot - 2); krcol = krcol + 1) {
+            //
+            // ==== Bulges number MTOP to MBOT are active double implicit
             // .    shift bulges.  There may or may not also be small
             // .    2-by-2 bulge, if there is room.  The inactive bulges
             // .    (if any) must wait until the active bulges have moved
             // .    down the diagonal to make room.  The phantom matrix
+            // .    paradigm described above helps keep track.  ====
             //
             mtop = max((INTEGER)1, (ktop - krcol) / 2 + 1);
             mbot = min(nbmps, (kbot - krcol - 1) / 2);
             m22 = mbot + 1;
             bmp22 = (mbot < nbmps) && (krcol + 2 * (m22 - 1)) == (kbot - 2);
+            //
+            // ==== Generate reflections to chase the chain right
+            // .    one column.  (The minimum value of K is KTOP-1.) ====
+            //
             if (bmp22) {
+                //
+                // ==== Special case: 2-by-2 reflection at bottom treated
+                // .    separately ====
                 //
                 k = krcol + 2 * (m22 - 1);
                 if (k == ktop - 1) {
@@ -136,11 +178,17 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     h[((k + 2) - 1) + (k - 1) * ldh] = zero;
                 }
                 //
+                // ==== Perform update from right within
+                // .    computational window. ====
+                //
                 for (j = jtop; j <= min(kbot, k + 3); j = j + 1) {
                     refsum = v[(m22 - 1) * ldv] * (h[(j - 1) + ((k + 1) - 1) * ldh] + v[(2 - 1) + (m22 - 1) * ldv] * h[(j - 1) + ((k + 2) - 1) * ldh]);
                     h[(j - 1) + ((k + 1) - 1) * ldh] = h[(j - 1) + ((k + 1) - 1) * ldh] - refsum;
                     h[(j - 1) + ((k + 2) - 1) * ldh] = h[(j - 1) + ((k + 2) - 1) * ldh] - refsum * conj(v[(2 - 1) + (m22 - 1) * ldv]);
                 }
+                //
+                // ==== Perform update from left within
+                // .    computational window. ====
                 //
                 if (accum) {
                     jbot = min(ndcol, kbot);
@@ -154,12 +202,15 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     h[((k + 1) - 1) + (j - 1) * ldh] = h[((k + 1) - 1) + (j - 1) * ldh] - refsum;
                     h[((k + 2) - 1) + (j - 1) * ldh] = h[((k + 2) - 1) + (j - 1) * ldh] - refsum * v[(2 - 1) + (m22 - 1) * ldv];
                 }
+                //
+                // ==== The following convergence test requires that
                 // .    the tradition small-compared-to-nearby-diagonals
                 // .    criterion and the Ahues & Tisseur (LAWN 122, 1997)
                 // .    criteria both be satisfied.  The latter improves
                 // .    accuracy in some examples. Falling back on an
                 // .    alternate convergence criterion when TST1 or TST2
                 // .    is zero (as done here) is traditional but probably
+                // .    unnecessary. ====
                 //
                 if (k >= ktop) {
                     if (h[((k + 1) - 1) + (k - 1) * ldh] != zero) {
@@ -198,6 +249,9 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                         }
                     }
                 }
+                //
+                // ==== Accumulate orthogonal transformations. ====
+                //
                 if (accum) {
                     kms = k - incol;
                     for (j = max((INTEGER)1, ktop - incol); j <= kdu; j = j + 1) {
@@ -213,6 +267,9 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     }
                 }
             }
+            //
+            // ==== Normal case: Chain of 3-by-3 reflections ====
+            //
             for (m = mbot; m >= mtop; m = m - 1) {
                 k = krcol + 2 * (m - 1);
                 if (k == ktop - 1) {
@@ -220,22 +277,43 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     alpha = v[(m - 1) * ldv];
                     Clarfg(3, alpha, &v[(2 - 1) + (m - 1) * ldv], 1, v[(m - 1) * ldv]);
                 } else {
+                    //
+                    // ==== Perform delayed transformation of row below
+                    // .    Mth bulge. Exploit fact that first two elements
+                    // .    of row are actually zero. ====
+                    //
                     refsum = v[(m - 1) * ldv] * v[(3 - 1) + (m - 1) * ldv] * h[((k + 3) - 1) + ((k + 2) - 1) * ldh];
                     h[((k + 3) - 1) + (k - 1) * ldh] = -refsum;
                     h[((k + 3) - 1) + ((k + 1) - 1) * ldh] = -refsum * conj(v[(2 - 1) + (m - 1) * ldv]);
                     h[((k + 3) - 1) + ((k + 2) - 1) * ldh] = h[((k + 3) - 1) + ((k + 2) - 1) * ldh] - refsum * conj(v[(3 - 1) + (m - 1) * ldv]);
+                    //
+                    // ==== Calculate reflection to move
+                    // .    Mth bulge one step. ====
+                    //
                     beta = h[((k + 1) - 1) + (k - 1) * ldh];
                     v[(2 - 1) + (m - 1) * ldv] = h[((k + 2) - 1) + (k - 1) * ldh];
                     v[(3 - 1) + (m - 1) * ldv] = h[((k + 3) - 1) + (k - 1) * ldh];
                     Clarfg(3, beta, &v[(2 - 1) + (m - 1) * ldv], 1, v[(m - 1) * ldv]);
+                    //
+                    // ==== A Bulge may collapse because of vigilant
+                    // .    deflation or destructive underflow.  In the
+                    // .    underflow case, try the two-small-subdiagonals
+                    // .    trick to try to reinflate the bulge.  ====
+                    //
                     if (h[((k + 3) - 1) + (k - 1) * ldh] != zero || h[((k + 3) - 1) + ((k + 1) - 1) * ldh] != zero || h[((k + 3) - 1) + ((k + 2) - 1) * ldh] == zero) {
+                        //
+                        // ==== Typical case: not collapsed (yet). ====
+                        //
                         h[((k + 1) - 1) + (k - 1) * ldh] = beta;
                         h[((k + 2) - 1) + (k - 1) * ldh] = zero;
                         h[((k + 3) - 1) + (k - 1) * ldh] = zero;
                     } else {
+                        //
+                        // ==== Atypical case: collapsed.  Attempt to
                         // .    reintroduce ignoring H(K+1,K) and H(K+2,K).
                         // .    If the fill resulting from the new
                         // .    reflector is too large, then abandon it.
+                        // .    Otherwise, use the new one. ====
                         //
                         Claqr1(3, &h[((k + 1) - 1) + ((k + 1) - 1) * ldh], ldh, s[(2 * m - 1) - 1], s[(2 * m) - 1], vt);
                         alpha = vt[0];
@@ -243,12 +321,20 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                         refsum = conj(vt[0]) * (h[((k + 1) - 1) + (k - 1) * ldh] + conj(vt[1]) * h[((k + 2) - 1) + (k - 1) * ldh]);
                         //
                         if (cabs1(h[((k + 2) - 1) + (k - 1) * ldh] - refsum * vt[1]) + cabs1(refsum * vt[2]) > ulp * (cabs1(h[(k - 1) + (k - 1) * ldh]) + cabs1(h[((k + 1) - 1) + ((k + 1) - 1) * ldh]) + cabs1(h[((k + 2) - 1) + ((k + 2) - 1) * ldh]))) {
+                            //
+                            // ==== Starting a new bulge here would
+                            // .    create non-negligible fill.  Use
+                            // .    the old one with trepidation. ====
+                            //
                             h[((k + 1) - 1) + (k - 1) * ldh] = beta;
                             h[((k + 2) - 1) + (k - 1) * ldh] = zero;
                             h[((k + 3) - 1) + (k - 1) * ldh] = zero;
                         } else {
+                            //
+                            // ==== Starting a new bulge here would
                             // .    create only negligible fill.
                             // .    Replace the old reflector with
+                            // .    the new one. ====
                             //
                             h[((k + 1) - 1) + (k - 1) * ldh] = h[((k + 1) - 1) + (k - 1) * ldh] - refsum;
                             h[((k + 2) - 1) + (k - 1) * ldh] = zero;
@@ -259,9 +345,12 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                         }
                     }
                 }
+                //
+                // ====  Apply reflection from the right and
                 // .     the first column of update from the left.
                 // .     These updates are required for the vigilant
                 // .     deflation check. We still delay most of the
+                // .     updates from the left for efficiency. ====
                 //
                 for (j = jtop; j <= min(kbot, k + 3); j = j + 1) {
                     refsum = v[(m - 1) * ldv] * (h[(j - 1) + ((k + 1) - 1) * ldh] + v[(2 - 1) + (m - 1) * ldv] * h[(j - 1) + ((k + 2) - 1) * ldh] + v[(3 - 1) + (m - 1) * ldv] * h[(j - 1) + ((k + 3) - 1) * ldh]);
@@ -269,16 +358,23 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     h[(j - 1) + ((k + 2) - 1) * ldh] = h[(j - 1) + ((k + 2) - 1) * ldh] - refsum * conj(v[(2 - 1) + (m - 1) * ldv]);
                     h[(j - 1) + ((k + 3) - 1) * ldh] = h[(j - 1) + ((k + 3) - 1) * ldh] - refsum * conj(v[(3 - 1) + (m - 1) * ldv]);
                 }
+                //
+                // ==== Perform update from left for subsequent
+                // .    column. ====
+                //
                 refsum = conj(v[(m - 1) * ldv]) * (h[((k + 1) - 1) + ((k + 1) - 1) * ldh] + conj(v[(2 - 1) + (m - 1) * ldv]) * h[((k + 2) - 1) + ((k + 1) - 1) * ldh] + conj(v[(3 - 1) + (m - 1) * ldv]) * h[((k + 3) - 1) + ((k + 1) - 1) * ldh]);
                 h[((k + 1) - 1) + ((k + 1) - 1) * ldh] = h[((k + 1) - 1) + ((k + 1) - 1) * ldh] - refsum;
                 h[((k + 2) - 1) + ((k + 1) - 1) * ldh] = h[((k + 2) - 1) + ((k + 1) - 1) * ldh] - refsum * v[(2 - 1) + (m - 1) * ldv];
                 h[((k + 3) - 1) + ((k + 1) - 1) * ldh] = h[((k + 3) - 1) + ((k + 1) - 1) * ldh] - refsum * v[(3 - 1) + (m - 1) * ldv];
+                //
+                // ==== The following convergence test requires that
                 // .    the tradition small-compared-to-nearby-diagonals
                 // .    criterion and the Ahues & Tisseur (LAWN 122, 1997)
                 // .    criteria both be satisfied.  The latter improves
                 // .    accuracy in some examples. Falling back on an
                 // .    alternate convergence criterion when TST1 or TST2
                 // .    is zero (as done here) is traditional but probably
+                // .    unnecessary. ====
                 //
                 if (k < ktop) {
                     continue;
@@ -319,6 +415,9 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     }
                 }
             }
+            //
+            // ==== Multiply H by reflections from the left ====
+            //
             if (accum) {
                 jbot = min(ndcol, kbot);
             } else if (wantt) {
@@ -336,8 +435,14 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     h[((k + 3) - 1) + (j - 1) * ldh] = h[((k + 3) - 1) + (j - 1) * ldh] - refsum * v[(3 - 1) + (m - 1) * ldv];
                 }
             }
+            //
+            // ==== Accumulate orthogonal transformations. ====
+            //
             if (accum) {
+                //
+                // ==== Accumulate U. (If needed, update Z later
                 // .    with an efficient matrix-matrix
+                // .    multiply.) ====
                 //
                 for (m = mbot; m >= mtop; m = m - 1) {
                     k = krcol + 2 * (m - 1);
@@ -353,7 +458,10 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     }
                 }
             } else if (wantz) {
+                //
+                // ==== U is not accumulated, so update Z
                 // .    now by multiplying by reflections
+                // .    from the right. ====
                 //
                 for (m = mbot; m >= mtop; m = m - 1) {
                     k = krcol + 2 * (m - 1);
@@ -365,8 +473,14 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
                     }
                 }
             }
+            //
+            // ==== End of near-the-diagonal bulge chase. ====
+            //
         }
+        //
+        // ==== Use U (if accumulated) to update far-from-diagonal
         // .    entries in H.  If required, use U to update Z as
+        // .    well. ====
         //
         if (accum) {
             if (wantt) {
@@ -378,16 +492,25 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
             }
             k1 = max((INTEGER)1, ktop - incol);
             nu = (kdu - max((INTEGER)0, ndcol - kbot)) - k1 + 1;
+            //
+            // ==== Horizontal Multiply ====
+            //
             for (jcol = min(ndcol, kbot) + 1; jcol <= jbot; jcol = jcol + nh) {
                 jlen = min(nh, jbot - jcol + 1);
                 Cgemm("C", "N", nu, jlen, nu, one, &u[(k1 - 1) + (k1 - 1) * ldu], ldu, &h[((incol + k1) - 1) + (jcol - 1) * ldh], ldh, zero, wh, ldwh);
                 Clacpy("ALL", nu, jlen, wh, ldwh, &h[((incol + k1) - 1) + (jcol - 1) * ldh], ldh);
             }
+            //
+            // ==== Vertical multiply ====
+            //
             for (jrow = jtop; jrow <= max(ktop, incol) - 1; jrow = jrow + nv) {
                 jlen = min(nv, max(ktop, incol) - jrow);
                 Cgemm("N", "N", jlen, nu, nu, one, &h[(jrow - 1) + ((incol + k1) - 1) * ldh], ldh, &u[(k1 - 1) + (k1 - 1) * ldu], ldu, zero, wv, ldwv);
                 Clacpy("ALL", jlen, nu, wv, ldwv, &h[(jrow - 1) + ((incol + k1) - 1) * ldh], ldh);
             }
+            //
+            // ==== Z multiply (also vertical) ====
+            //
             if (wantz) {
                 for (jrow = iloz; jrow <= ihiz; jrow = jrow + nv) {
                     jlen = min(nv, ihiz - jrow + 1);
@@ -397,4 +520,7 @@ void Claqr5(bool const wantt, bool const wantz, INTEGER const kacc22, INTEGER co
             }
         }
     }
+    //
+    // ==== End of Claqr5 ====
+    //
 }
