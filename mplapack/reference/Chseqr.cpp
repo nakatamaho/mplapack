b63d8f3a8b9de45d823f8faf2398929bc13c272f
NAKATA Maho <maho.nakata@gmail.com>
2025-12-12 11:51:32 +0900

    .

--- mplapack/reference/Chseqr.cpp
+++ mplapack/reference/Chseqr.cpp
@@ -151,8 +151,8 @@ void Chseqr(const char *job, const char *compz, INTEGER const n, INTEGER const i
                     // .    array before calling Claqr0. ====
                     //
                     Clacpy("A", n, n, h, ldh, hl, nl);
-                    hl[((n + 1) - 1) + (n - 1) * ldhl] = zero;
-                    Claset("A", nl, nl - n, zero, zero, &hl[((n + 1) - 1) * ldhl], nl);
+                    hl[((n + 1) - 1) + (n - 1) * nl] = zero;
+                    Claset("A", nl, nl - n, zero, zero, &hl[((n + 1) - 1) * nl], nl);
                     Claqr0(wantt, wantz, nl, ilo, kbot, hl, nl, w, ilo, ihi, z, ldz, workl, nl, info);
                     if (wantt || info != 0) {
                         Clacpy("A", n, n, hl, nl, h, ldh);
--- mplapack/reference/Claed0.cpp
+++ mplapack/reference/Claed0.cpp
@@ -163,7 +163,7 @@ statement_10:
         }
         ll = iq - 1 + iwork[(iqptr + curr) - 1];
         Rsteqr("I", matsiz, &d[submat - 1], &e[submat - 1], &rwork[ll - 1], matsiz, rwork, info);
-        Clacrm(qsiz, matsiz, &q[(submat - 1) * ldq], ldq, &rwork[ll - 1], matsiz, &qstore[(submat - 1) * ldqstore], ldqs, &rwork[iwrem - 1]);
+        Clacrm(qsiz, matsiz, &q[(submat - 1) * ldq], ldq, &rwork[ll - 1], matsiz, &qstore[(submat - 1) * ldqs], ldqs, &rwork[iwrem - 1]);
         iwork[(iqptr + curr + 1) - 1] = iwork[(iqptr + curr) - 1] + pow2(matsiz);
         curr++;
         if (info > 0) {
@@ -206,7 +206,7 @@ statement_80:
             //
             // I am free to use Q as a valuable working space until Loop 150.
             //
-            Claed7(matsiz, msd2, qsiz, tlvls, curlvl, curprb, &d[submat - 1], &qstore[(submat - 1) * ldqstore], ldqs, e[(submat + msd2 - 1) - 1], &iwork[(indxq + submat) - 1], &rwork[iq - 1], &iwork[iqptr - 1], &iwork[iprmpt - 1], &iwork[iperm - 1], &iwork[igivpt - 1], &iwork[igivcl - 1], &rwork[igivnm - 1], &q[(submat - 1) * ldq], &rwork[iwrem - 1], &iwork[(subpbs + 1) - 1], info);
+            Claed7(matsiz, msd2, qsiz, tlvls, curlvl, curprb, &d[submat - 1], &qstore[(submat - 1) * ldqs], ldqs, e[(submat + msd2 - 1) - 1], &iwork[(indxq + submat) - 1], &rwork[iq - 1], &iwork[iqptr - 1], &iwork[iprmpt - 1], &iwork[iperm - 1], &iwork[igivpt - 1], &iwork[igivcl - 1], &rwork[igivnm - 1], &q[(submat - 1) * ldq], &rwork[iwrem - 1], &iwork[(subpbs + 1) - 1], info);
             if (info > 0) {
                 info = submat * (n + 1) + submat + matsiz - 1;
                 return;
@@ -226,7 +226,7 @@ statement_80:
     for (i = 1; i <= n; i = i + 1) {
         j = iwork[(indxq + i) - 1];
         rwork[i - 1] = d[j - 1];
-        Ccopy(qsiz, &qstore[(j - 1) * ldqstore], 1, &q[(i - 1) * ldq], 1);
+        Ccopy(qsiz, &qstore[(j - 1) * ldqs], 1, &q[(i - 1) * ldq], 1);
     }
     Rcopy(n, rwork, 1, d, 1);
     //
