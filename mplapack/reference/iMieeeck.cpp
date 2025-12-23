39b24fc22e98367f9db27924adb464bf9471b40d
NAKATA Maho <maho.nakata@gmail.com>
2025-12-23 18:21:02 +0900

    .

--- mplapack/reference/iMieeeck.cpp
+++ mplapack/reference/iMieeeck.cpp
@@ -38,6 +38,18 @@
 
 INTEGER iMieeeck(INTEGER const ispec, REAL const zero, REAL const one) {
     INTEGER return_value = 0;
+#if defined ___MPLAPACK_BUILD_WITH_GMP___
+    // GMP is not a natural extention to IEEE 754.
+    return 0;
+#endif
+#if defined ___MPLAPACK_BUILD_WITH_DD___
+    // DD does not comply IEEE 754.
+    return 0;
+#endif
+#if defined ___MPLAPACK_BUILD_WITH_QD___
+    // DD does not comply IEEE 754.
+    return 0;
+#endif
     return_value = 1;
     //
     REAL posinf = one / zero;
