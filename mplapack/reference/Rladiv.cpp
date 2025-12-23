2abe40af81ac555ff7eed3f43b6e3c57bf1b1c8c
NAKATA Maho <maho.nakata@gmail.com>
2025-12-23 18:24:06 +0900

    .

--- mplapack/reference/Rladiv.cpp
+++ mplapack/reference/Rladiv.cpp
@@ -36,7 +36,7 @@
 #include <mpblas.h>
 #include <mplapack.h>
 
-REAL Rladiv2(REAL const &a, REAL const &b, REAL const &c, REAL const &d, REAL const &r, REAL const &t) {
+REAL Rladiv2(REAL const a, REAL const b, REAL const c, REAL const d, REAL const r, REAL const t) {
     REAL return_value = 0.0;
     const REAL zero = 0.0;
     REAL br = 0.0;
@@ -57,7 +57,7 @@ REAL Rladiv2(REAL const &a, REAL const &b, REAL const &c, REAL const &d, REAL co
     //
 }
 
-void Rladiv1(REAL &a, REAL const &b, REAL const &c, REAL const &d, REAL &p, REAL &q) {
+void Rladiv1(REAL &a, REAL const b, REAL const c, REAL const d, REAL &p, REAL &q) {
     REAL r = d / c;
     const REAL one = 1.0;
     REAL t = one / (c + d * r);
@@ -69,7 +69,7 @@ void Rladiv1(REAL &a, REAL const &b, REAL const &c, REAL const &d, REAL &p, REAL
     //
 }
 
-void Rladiv(REAL const &a, REAL const &b, REAL const &c, REAL const &d, REAL &p, REAL &q) {
+void Rladiv(REAL const a, REAL const b, REAL const c, REAL const d, REAL &p, REAL &q) {
     REAL aa = a;
     REAL bb = b;
     REAL cc = c;
