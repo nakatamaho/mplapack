3a3a6d7d1e60126e33330aaf185bb9c6c6be4523
NAKATA Maho <maho.nakata@gmail.com>
2025-12-25 15:14:34 +0900

    .

--- mplapack/reference/Clargv.cpp
+++ mplapack/reference/Clargv.cpp
@@ -36,6 +36,12 @@
 #include <mpblas.h>
 #include <mplapack.h>
 
+inline REAL abssq(COMPLEX ff) {
+    REAL temp;
+    temp = (ff.real() * ff.real()) + (ff.imag() * ff.imag());
+    return temp;
+}
+
 void Clargv(INTEGER const n, COMPLEX *x, INTEGER const incx, COMPLEX *y, INTEGER const incy, REAL *c, INTEGER const incc) {
     COMPLEX ff = 0.0;
     REAL safmin = 0.0;
--- mplapack/reference/Clartg.cpp
+++ mplapack/reference/Clartg.cpp
@@ -36,6 +36,12 @@
 #include <mpblas.h>
 #include <mplapack.h>
 
+inline REAL abssq(COMPLEX ff) {
+    REAL temp;
+    temp = (ff.real() * ff.real()) + (ff.imag() * ff.imag());
+    return temp;
+}
+
 void Clartg(COMPLEX const f, COMPLEX const g, REAL &cs, COMPLEX &sn, COMPLEX &r) {
     COMPLEX ff = 0.0;
     REAL safmin = 0.0;
