4009d9e20f6717c7acad3b99a0a86dc7c2072802
NAKATA Maho <maho.nakata@gmail.com>
2025-12-25 16:31:09 +0900

    .

--- mplapack/reference/Clasyf_aa.cpp
+++ mplapack/reference/Clasyf_aa.cpp
@@ -129,7 +129,7 @@ void Clasyf_aa(const char *uplo, INTEGER const j1, INTEGER const m, INTEGER cons
             //
             // Apply symmetric pivot
             //
-            if ((i2 != 2) && (piv != 0)) {
+            if ((i2 != 2) && (piv != zero)) {
                 //
                 // Swap WORK(I1) and WORK(I2)
                 //
@@ -273,7 +273,7 @@ void Clasyf_aa(const char *uplo, INTEGER const j1, INTEGER const m, INTEGER cons
             //
             // Apply symmetric pivot
             //
-            if ((i2 != 2) && (piv != 0)) {
+            if ((i2 != 2) && (piv != zero)) {
                 //
                 // Swap WORK(I1) and WORK(I2)
                 //
