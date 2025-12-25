c5075abbde6066129ca79a3b16fb181a36963036
NAKATA Maho <maho.nakata@gmail.com>
2025-12-25 13:59:56 +0900

    .

--- mplapack/reference/Cheequb.cpp
+++ mplapack/reference/Cheequb.cpp
@@ -41,6 +41,7 @@ void Cheequb(const char *uplo, INTEGER const n, COMPLEX *a, INTEGER const lda, R
     bool up = false;
     const REAL zero = 0.0;
     const REAL one = 1.0;
+    const REAL two = 2.0;    
     INTEGER i = 0;
     INTEGER j = 0;
     REAL tol = 0.0;
@@ -118,7 +119,7 @@ void Cheequb(const char *uplo, INTEGER const n, COMPLEX *a, INTEGER const lda, R
         s[j - 1] = 1.0 / s[j - 1];
     }
     //
-    tol = one / sqrt(2.0 * n);
+    tol = one / sqrt(two * castREAL(n));
     //
     for (iter = 1; iter <= max_iter; iter = iter + 1) {
         scale = 0.0;
