00873fe494f00618ca7aa55f23e15e32eba6dd5b
NAKATA Maho <maho.nakata@gmail.com>
2025-12-25 14:00:19 +0900

    .

--- fable/patch_lapack.sh
+++ fable/patch_lapack.sh
@@ -33,3 +33,4 @@ patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-iMlaenv.cpp
 patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-iMparmq.cpp
 patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-Cgttrs.cpp
 patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-Cggev3.cpp
+patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-Cheequb.cpp
