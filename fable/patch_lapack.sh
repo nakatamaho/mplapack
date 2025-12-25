775918ab15fde819d74c7216680843b1479b8812
NAKATA Maho <maho.nakata@gmail.com>
2025-12-25 17:37:26 +0900

    .

--- fable/patch_lapack.sh
+++ fable/patch_lapack.sh
@@ -30,7 +30,7 @@ patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-iMieeeck.cpp
 patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-Rladiv.cpp
 patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Cgttrs.cpp
 patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Cggev3.cpp
-patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Cheequb.cpp
+#patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Cheequb.cpp
 patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Cggev.cpp
 patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Cggevx.cpp
 patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-Clags2.cpp
@@ -43,3 +43,4 @@ patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-iMlaenv.cpp
 patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-iMparmq.cpp
 patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Clatrs.cpp
 patch -p3    < ~/mplapack/fable/3.9.1/lapack/patch-Csytri2x.cpp
+patch -p3 -R < ~/mplapack/fable/3.9.1/lapack/patch-Cheequb.cpp
