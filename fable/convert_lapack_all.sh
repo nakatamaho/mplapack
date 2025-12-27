bbb3a21bf0dadea1a9fc7d9bae5a041dbdf6adfd
NAKATA Maho <maho.nakata@gmail.com>
2025-12-28 06:48:57 +0900

    .

--- fable/convert_lapack_all.sh
+++ fable/convert_lapack_all.sh
@@ -18,14 +18,28 @@ EXCLUDE_PREFIXES=( s c )
 EXCLUDE_BASENAMES_MANUAL=( )
 
 # Basenames that are not needed on the C++ side
-EXCLUDE_BASENAMES_UNUSED=( la_constants dsgesv zcgesv dsposv zcposv dla_gerfsx_extended dla_gbrfsx_extended dla_porfsx_extended dla_syrfsx_extended zla_gerfsx_extended zla_gbrfsx_extended zla_porfsx_extended zla_syrfsx_extended zla_herfsx_extended lsamen ilaslr ilaslc ilaclc ilaclr iladiag icmax1 xerbla xerbla_array)
-
+EXCLUDE_BASENAMES_UNUSED=( la_constants dsgesv zcgesv dsposv zcposv dla_gerfsx_extended dla_gbrfsx_extended dla_porfsx_extended dla_syrfsx_extended zla_gerfsx_extended zla_gbrfsx_extended zla_porfsx_extended zla_syrfsx_extended zla_herfsx_extended lsamen ilaslr ilaslc ilaclc ilaclr iladiag icmax1 xerbla xerbla_array zgbrfsx zgerfsx zherfsx zporfsx zsyrfsx dgerfsx dgbrfsx dporfsx dsyrfsx clag2c clat2c dlag2s dlat2s)
 # lsamen is not used?
 # ilaslr is only used in slarf.
 # iladiag is not used?
 # ilaslc is not used?
 # ilaclc is only used in clarf.
 # ilaclr is only used in clarf.
+# The following ones use xBLAS so we do not support them.
+# zgbrfsx
+# zgerfsx
+# zherfsx
+# zporfsx
+# zsyrfsx
+# dgerfsx
+# dgbrfsx
+# dporfsx
+# dsyrfsx
+# The following ones converts to a lower precision, so we dont need them.
+# clag2c
+# clat2c
+# dlag2s
+# dlat2s
 
 # Other helper/utility routines to be excluded from this pass
 EXCLUDE_BASENAMES_MISC=( dlaruv )
