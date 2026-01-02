d5e514a1b899bbe5a18f979dbe58820218e19bb3
NAKATA Maho <maho.nakata@gmail.com>
2026-01-02 17:54:23 +0900

    Add guard for Rlarrd.cpp with MPFR.

--- mplapack/reference/Rlarrd.cpp
+++ mplapack/reference/Rlarrd.cpp
@@ -201,6 +201,10 @@ void Rlarrd(const char *range, const char *order, INTEGER const n, REAL const vl
         // IL through IU. The initial interval [GL,GU] from the global
         // Gerschgorin bounds GL and GU is refined by Rlaebz.
         itmax = castINTEGER((log(tnorm + pivmin) - log(pivmin)) / log(two)) + 2;
+#if defined ___MPLAPACK_BUILD_WITH_MPFR___
+        if (itmax >= 1024)
+            itmax = 1024; // XXX itmax can be too large for MPFR (=10^8)
+#endif
         work[(n + 1) - 1] = gl;
         work[(n + 2) - 1] = gl;
         work[(n + 3) - 1] = gu;
