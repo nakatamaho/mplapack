a0dea25d728cc3e58a1e29150aa8cb5fb3433cd3
NAKATA Maho <maho.nakata@gmail.com>
2026-01-18 08:58:43 +0900

    .

---
+++ fable/patch_lapack_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd /home/docker/mplapack/mplapack/test/matgen
+
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Clatm5.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rlatmt.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rlatms.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Clatms.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Clarnd.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Claror.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rlarnd.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rlarnd.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rlaror.cpp
