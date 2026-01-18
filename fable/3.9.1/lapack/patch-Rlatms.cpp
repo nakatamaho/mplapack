d892543fa088153bf61d7f30828004c735cfd536
NAKATA Maho <maho.nakata@gmail.com>
2026-01-18 09:36:27 +0900

    .

--- fable/3.9.1/lapack/patch-Rlatms.cpp
+++ fable/3.9.1/lapack/patch-Rlatms.cpp
@@ -6,7 +6,7 @@
      REAL extra = 0.0;
 -    const REAL twopi = 6.28318530717958647692528676655900576839;
 +    const REAL two = 2.0;
-+    const REAL twopi = pi(zero) * two;
++    const REAL twopi = two * pi(zero);
      REAL angle = 0.0;
      REAL c = 0.0;
      REAL s = 0.0;
