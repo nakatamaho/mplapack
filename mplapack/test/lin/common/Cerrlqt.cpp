eb02b19dfc9145eb1f7ad644e4c49e50a595e552
NAKATA Maho <maho.nakata@gmail.com>
2026-01-19 17:28:21 +0900

    .

--- mplapack/test/lin/common/Cerrlqt.cpp
+++ mplapack/test/lin/common/Cerrlqt.cpp
@@ -59,13 +59,15 @@ void Cerrlqt(fem::str_cref path, INTEGER const nunit) {
     COMPLEX c[nmax * nmax];
     COMPLEX t[nmax * nmax];
     COMPLEX w[nmax];
+    REAL zero = 0.0;
+    REAL one = 1.0;
     for (j = 1; j <= nmax; j = j + 1) {
         for (i = 1; i <= nmax; i = i + 1) {
-            a[(i - 1) + (j - 1) * nmax] = 1.0 / COMPLEX(castREAL(i + j), 0.0);
-            c[(i - 1) + (j - 1) * nmax] = 1.0 / COMPLEX(castREAL(i + j), 0.0);
-            t[(i - 1) + (j - 1) * nmax] = 1.0 / COMPLEX(castREAL(i + j), 0.0);
+            a[(i - 1) + (j - 1) * nmax] = one / COMPLEX(castREAL(i + j), 0.0);
+            c[(i - 1) + (j - 1) * nmax] = one / COMPLEX(castREAL(i + j), 0.0);
+            t[(i - 1) + (j - 1) * nmax] = one / COMPLEX(castREAL(i + j), 0.0);
         }
-        w[j - 1] = 0.0;
+        w[j - 1] = zero;
     }
     ok = true;
     //
