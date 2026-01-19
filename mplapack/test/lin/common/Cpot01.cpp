c902d50e9187160bcc90179d7851e7b07f7c3ff0
NAKATA Maho <maho.nakata@gmail.com>
2026-01-19 20:03:09 +0900

    .

--- mplapack/test/lin/common/Cpot01.cpp
+++ mplapack/test/lin/common/Cpot01.cpp
@@ -84,7 +84,7 @@ void Cpot01(fem::str_cref uplo, INTEGER const n, COMPLEX *a, INTEGER const lda, 
             //
             // Compute the (K,K) element of the result.
             //
-            tr = Cdotc(k, &afac[(k - 1) * ldafac], 1, &afac[(k - 1) * ldafac], 1);
+            tr = Cdotc(k, &afac[(k - 1) * ldafac], 1, &afac[(k - 1) * ldafac], 1).real();
             afac[(k - 1) + (k - 1) * ldafac] = tr;
             //
             // Compute the rest of column K.
