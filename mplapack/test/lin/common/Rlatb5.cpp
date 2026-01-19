82630f0dc4c5fe90ab408ea4134dd9e25d59e814
NAKATA Maho <maho.nakata@gmail.com>
2026-01-19 20:00:38 +0900

    .

--- mplapack/test/lin/common/Rlatb5.cpp
+++ mplapack/test/lin/common/Rlatb5.cpp
@@ -44,45 +44,36 @@ using fem::common;
 #include <mplapack_lin.h>
 
 void Rlatb5(fem::str_cref path, INTEGER const imat, INTEGER const n, fem::str_ref type, INTEGER &kl, INTEGER &ku, REAL &anorm, INTEGER &mode, REAL &cndnum, fem::str_ref dist) {
-    common cmn;
-    static bool first = true;
-    double &badc1 = sve.badc1;
-    double &badc2 = sve.badc2;
-    double &eps = sve.eps;
-    double &large = sve.large;
-    double &small = sve.small;
     //
     // Set some constants for use in the subroutine.
     //
     const REAL tenth = 0.1;
     const REAL one = 1.0;
     const REAL shrink = 0.25;
-    if (first) {
-        first = false;
-        eps = Rlamch("Precision");
-        badc2 = tenth / eps;
-        badc1 = sqrt(badc2);
-        small = Rlamch("Safe minimum");
-        large = one / small;
-        //
-        // If it looks like we're on a Cray, take the square root of
-        // SMALL and LARGE to avoid overflow and underflow problems.
-        //
-        Rlabad(small, large);
-        small = shrink * (small / eps);
-        large = one / small;
-    }
+    first = false;
+    eps = Rlamch("Precision");
+    badc2 = tenth / eps;
+    badc1 = sqrt(badc2);
+    small = Rlamch("Safe minimum");
+    large = one / small;
+    //
+    // If it looks like we're on a Cray, take the square root of
+    // SMALL and LARGE to avoid overflow and underflow problems.
+    //
+    Rlabad(small, large);
+    small = shrink * (small / eps);
+    large = one / small;
     //
     fem::str<2> c2 = path(2, 3);
     //
     // Set some parameters
     //
-    *dist = 'S';
+    dist = "S";
     mode = 3;
     //
     // Set TYPE, the type of matrix to be generated.
     //
-    *type = c2(1, 1);
+    type = c2(1, 1);
     //
     // Set the lower and upper bandwidths.
     //
