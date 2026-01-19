d7d4f356b33a59d343ec154199bfac223af257ea
NAKATA Maho <maho.nakata@gmail.com>
2026-01-20 07:36:32 +0900

    .

--- mplapack/test/lin/common/Rdrvrf3.cpp
+++ mplapack/test/lin/common/Rdrvrf3.cpp
@@ -80,9 +80,9 @@ void Rdrvrf3(INTEGER const nout, INTEGER const nn, INTEGER *nval, REAL const thr
     INTEGER idiag = 0;
     fem::str<1> diag;
     INTEGER ialpha = 0;
-    const REAL zero = COMPLEX(0.0, 0.0);
+    const REAL zero = 0.0;
     REAL alpha = 0.0;
-    const REAL one = COMPLEX(1.0, 0.0);
+    const REAL one = 1.0;
     INTEGER na = 0;
     INTEGER j = 0;
     const INTEGER ntests = 1;
