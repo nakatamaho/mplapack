88ef8dea01ef9e2de4d8d76ba52273257b0b50d8
NAKATA Maho <maho.nakata@gmail.com>
2026-01-20 12:59:02 +0900

    .

--- mplapack/test/lin/common/Cchkaa.cpp
+++ mplapack/test/lin/common/Cchkaa.cpp
@@ -43,8 +43,7 @@ using fem::common;
 #include <mplapack_matgen.h>
 #include <mplapack_lin.h>
 
-void program_zchkaa(int argc, char const *argv[]) {
-    common cmn(argc, argv);
+void Cchkaa(void) {
     common_read read(cmn);
     common_write write(cmn);
     static fem::str<10> intstr = "0123456789";
@@ -125,10 +124,10 @@ void program_zchkaa(int argc, char const *argv[]) {
     //
     // Report values of parameters.
     //
-    ilaver(vers_major, vers_minor, vers_patch);
-    write(nout, "(' Tests of the COMPLEX*16 LAPACK routines ',/,' LAPACK VERSION ',i1,'.',"
-                "i1,'.',i1,/,/,' The following parameter values will be used:')"),
-        vers_major, vers_minor, vers_patch;
+    iMlaver(mplapack_vers_major, mplapack_vers_minor, mplapack_vers_patch, lapack_vers_major, lapack_vers_minor, lapack_vers_patch);
+    write(nout, "(' Tests of the Multiple precision version of LAPACK MPLAPACK VERSION ',i1,'.',i1,'.',i1,/, "
+                "' Based on the original LAPACK VERSION ',i1,'.',i1,'.',i1,/,/, 'The following parameter values will be used:')"),
+        mplapack_vers_major, mplapack_vers_minor, mplapack_vers_patch, lapack_vers_major, lapack_vers_minor, lapack_vers_patch;
     //
     // Read the values of M
     //
@@ -1078,4 +1077,4 @@ statement_140:
     //
 }
 
-int main(int argc, char const *argv[]) { return fem::main_with_catch(argc, argv, program_zchkaa); }
+int main(int argc, char const *argv[]) { return Cchkaa(); }
--- mplapack/test/lin/common/Rchkaa.cpp
+++ mplapack/test/lin/common/Rchkaa.cpp
@@ -43,8 +43,7 @@ using fem::common;
 #include <mplapack_matgen.h>
 #include <mplapack_lin.h>
 
-void program_dchkaa(int argc, char const *argv[]) {
-    common cmn(argc, argv);
+void Rchkaa(void) {
     common_read read(cmn);
     common_write write(cmn);
     static fem::str<10> intstr = "0123456789";
@@ -934,4 +933,4 @@ statement_140:
     //
 }
 
-int main(int argc, char const *argv[]) { return fem::main_with_catch(argc, argv, program_dchkaa); }
+int main(int argc, char const *argv[]) { return Rchkaa(); }
