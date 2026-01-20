85e8c31bc9f0797c0400f40095b3ad40c08333d0
NAKATA Maho <maho.nakata@gmail.com>
2026-01-20 11:24:39 +0900

    indent

--- fable/fem/utils/token.hpp
+++ fable/fem/utils/token.hpp
@@ -3,30 +3,19 @@
 
 #include <string>
 
-namespace fem { namespace utils {
+namespace fem {
+namespace utils {
 
-  struct token
-  {
-    std::string type;
-    std::string value;
+    struct token {
+        std::string type;
+        std::string value;
 
-    token(
-      std::string type_,
-      std::string value_)
-    :
-      type(type_),
-      value(value_)
-    {}
+        token(std::string type_, std::string value_) : type(type_), value(value_) {}
 
-    token(
-      std::string type_,
-      char value_)
-    :
-      type(type_),
-      value(1, value_)
-    {}
-  };
+        token(std::string type_, char value_) : type(type_), value(1, value_) {}
+    };
 
-}} // namespace fem::utils
+} // namespace utils
+} // namespace fem
 
 #endif // GUARD
