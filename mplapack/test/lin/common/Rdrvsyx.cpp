ac3a717befe28a35680c01791b922a0eebfc6ca6
NAKATA Maho <maho.nakata@gmail.com>
2026-01-21 17:19:41 +0900

    .

--- mplapack/test/lin/common/Rdrvsyx.cpp
+++ mplapack/test/lin/common/Rdrvsyx.cpp
@@ -42,6 +42,7 @@ using fem::common;
 
 #include <mplapack_matgen.h>
 #include <mplapack_lin.h>
+#include <memory>
 
 void Rdrvsy(bool *dotype, INTEGER const nn, INTEGER *nval, INTEGER const nrhs, REAL const thresh, bool const tsterr, INTEGER const nmax, REAL *a, REAL *afac, REAL *ainv, REAL *b, REAL *x, REAL *xact, REAL *work, REAL *rwork, INTEGER *iwork, INTEGER const nout) {
     common cmn;
