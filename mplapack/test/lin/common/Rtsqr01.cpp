847dc14855022c316252bca5216b031b771c066c
NAKATA Maho <maho.nakata@gmail.com>
2026-01-21 21:22:27 +0900

    remove unncessary common cmn;

--- mplapack/test/lin/common/Rtsqr01.cpp
+++ mplapack/test/lin/common/Rtsqr01.cpp
@@ -45,7 +45,6 @@ using fem::common;
 #include <memory>
 
 void Rtsqr01(fem::str_cref tssw, INTEGER const m, INTEGER const n, INTEGER const mb, INTEGER const nb, REAL *result) {
-    common cmn;
     static INTEGER iseed[4] = {1988, 1989, 1990, 1991};
     // TEST TALL SKINNY OR SHORT WIDE
     //
