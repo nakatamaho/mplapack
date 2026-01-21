74afa76c06ca57035b3e71d041b9c50ce6350902
NAKATA Maho <maho.nakata@gmail.com>
2026-01-21 17:20:03 +0900

    .

--- mplapack/test/lin/common/Cdrvsyx.cpp
+++ mplapack/test/lin/common/Cdrvsyx.cpp
@@ -42,6 +42,7 @@ using fem::common;
 
 #include <mplapack_matgen.h>
 #include <mplapack_lin.h>
+#include <memory>
 
 void Cdrvsy(bool *dotype, INTEGER const nn, INTEGER *nval, INTEGER const nrhs, REAL const thresh, bool const tsterr, INTEGER const nmax, COMPLEX *a, COMPLEX *afac, COMPLEX *ainv, COMPLEX *b, COMPLEX *x, COMPLEX *xact, COMPLEX *work, REAL *rwork, INTEGER *iwork, INTEGER const nout) {
     common cmn;
