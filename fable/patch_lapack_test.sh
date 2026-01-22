b4055c8e6cd05e8825763e2da1bdd4f619a05e25
NAKATA Maho <maho.nakata@gmail.com>
2026-01-22 21:05:00 +0900

    .

--- fable/patch_lapack_test.sh
+++ fable/patch_lapack_test.sh
@@ -70,3 +70,4 @@ patch  < ~/mplapack/fable/3.9.1/lapack/patch-Cchkgg.cpp
 patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rdrgsx.cpp
 patch  < ~/mplapack/fable/3.9.1/lapack/patch-Cdrgsx.cpp
 patch  < ~/mplapack/fable/3.9.1/lapack/patch-Rchkee.cpp
+patch  < ~/mplapack/fable/3.9.1/lapack/patch-Cchkee.cpp
