18ff710ed74d1966a6f3ad6294a5ffebd0055981
NAKATA Maho <maho.nakata@gmail.com>
2026-01-25 14:35:16 +0900

    .

--- fable/3.9.1/lapack/patch-Cchkaa.cpp
+++ fable/3.9.1/lapack/patch-Cchkaa.cpp
@@ -84,6 +84,15 @@
      //
      // Read the values of M
      //
+@@ -446,7 +446,7 @@
+     //
+     // Check first character for correct precision.
+     //
+-    if (!Mlsame(c1.elems, "Zomplex precision")) {
++    if (!Mlsame(c1.elems, "Zomplex precision") && !Mlsame(c1.elems, "C")) {
+         write(nout, format_9990), path;
+         //
+     } else if (nmats <= 0) {
 @@ -1082,4 +1095,4 @@
      //
  }
