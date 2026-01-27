850b9431cb192ab92ac8a05de31a53162d81e669
NAKATA Maho <maho.nakata@gmail.com>
2026-01-27 17:37:13 +0900

    fix allocation

--- mplapack/test/lin/common/Rdrvls.cpp
+++ mplapack/test/lin/common/Rdrvls.cpp
@@ -94,6 +94,8 @@ void Rdrvls(bool *dotype, INTEGER const nm, INTEGER *mval, INTEGER const nn, INT
     INTEGER nrows = 0;
     INTEGER ncols = 0;
     INTEGER ldwork = 0;
+    std::unique_ptr<REAL[]> work_storage(new REAL[lwork]);
+    REAL *work = nullptr;
     const REAL one = 1.0;
     const INTEGER ntests = 16;
     REAL result[ntests];
@@ -102,6 +104,8 @@ void Rdrvls(bool *dotype, INTEGER const nm, INTEGER *mval, INTEGER const nn, INT
     INTEGER rank = 0;
     REAL normb = 0.0;
     INTEGER j = 0;
+    std::unique_ptr<INTEGER[]> iwork_storage(new INTEGER[liwork]);
+    INTEGER *iwork = nullptr;
     //
     static const char *format_9999 = "(' TRANS=''',a1,''', M=',i5,', N=',i5,', NRHS=',i4,', NB=',i4,', type',"
                                      "i2,', test(',i2,')=',g12.5)";
@@ -228,10 +232,10 @@ void Rdrvls(bool *dotype, INTEGER const nm, INTEGER *mval, INTEGER const nn, INT
     //
     lwlsy = lwork;
     //
-    std::unique_ptr<REAL[]> work_storage(new REAL[lwork]);
-    REAL *work = work_storage.get();
-    std::unique_ptr<INTEGER[]> iwork_storage(new INTEGER[liwork]);
-    INTEGER *iwork = iwork_storage.get();
+    work_storage.reset(new REAL[max((INTEGER)1, lwork)]);
+    work = work_storage.get();
+    iwork_storage.reset(new INTEGER[max((INTEGER)1, liwork)]);
+    iwork = iwork_storage.get();
     for (im = 1; im <= nm; im = im + 1) {
         m = mval[im - 1];
         lda = max((INTEGER)1, m);
