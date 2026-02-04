c84699ad70901e154a961390af42204fa9adb37d
NAKATA Maho <maho.nakata@gmail.com>
2026-02-04 18:08:40 +0900

    build: update macOS build script for gcc-14 and arm64 support
    - Upgrade from gcc-mp-10 to gcc-mp-14
    - Add architecture detection for _Float64x support:
      - x86_64/amd64: enable _Float64x
      - arm64/aarch64: disable _Float64x (not available on Apple Silicon)
    - Refactor configure flags into CONFIG_FLAGS array for maintainability
    - Remove redundant aclocal/autoconf/automake calls before autoreconf

--- misc/reconfig.macOS.sh
+++ misc/reconfig.macOS.sh
@@ -3,19 +3,43 @@
 USE_CCACHE=yes
 
 if [ x$USE_CCACHE = x"yes" ] ; then
-CXX="ccache g++-mp-10" ; export CXX
-CC="ccache gcc-mp-10" ; export CC
-FC="gfortran-mp-10"; export FC
+CXX="ccache g++-mp-14" ; export CXX
+CC="ccache gcc-mp-14" ; export CC
+FC="gfortran-mp-14"; export FC
 ccache -M 60G
 else
-CXX="g++-mp-10" ; export CXX
-CC="gcc-mp-10" ; export CC
-FC="gfortran-mp-10"; export FC
+CXX="g++-mp-14" ; export CXX
+CC="gcc-mp-14" ; export CC
+FC="gfortran-mp-14"; export FC
 fi
 
 pushd mplapack/test/compare ; bash gen.Makefile.am.sh ; popd
-
-aclocal ; autoconf ; automake --add-missing
 autoreconf --force --install
 
-./configure --prefix=$HOME/MPLAPACK --enable-gmp=yes --enable-mpfr=yes --enable-_Float128=yes --enable-qd=yes --enable-dd=yes --enable-double=yes --enable-_Float64x=yes --enable-test=yes --enable-benchmark=yes
+CONFIG_FLAGS=(
+  "--prefix=$HOME/MPLAPACK"
+  "--enable-gmp=yes"
+  "--enable-mpfr=yes"
+  "--enable-_Float128=yes"
+  "--enable-qd=yes"
+  "--enable-dd=yes"
+  "--enable-double=yes"
+  "--enable-test=yes"
+  "--enable-benchmark=yes"
+)
+
+arch="$(uname -m)"
+case "$arch" in
+  x86_64|amd64)
+    CONFIG_FLAGS+=("--enable-_Float64x=yes")
+    ;;
+  arm64|aarch64)
+    CONFIG_FLAGS+=("--enable-_Float64x=no")
+    ;;
+  *)
+    echo "Unknown arch: $arch (disabling _Float64x by default)" >&2
+    CONFIG_FLAGS+=("--enable-_Float64x=no")
+    ;;
+esac
+
+./configure "${CONFIG_FLAGS[@]}"
