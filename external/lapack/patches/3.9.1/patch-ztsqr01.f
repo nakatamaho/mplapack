6ed8d54ddf1d61d10c9f879b496cab304e38bac2
NAKATA Maho <maho.nakata@gmail.com>
2026-04-11 14:47:27 +0900

    external/lapack: fix version override, unify patch application to -p1
    - Replace ?= with $(or ...) for LAPACK_VERSION/LAPACK_GIT_URL/LAPACK_GIT_REF
      so that env var override (e.g. LAPACK_VERSION=3.9.1 make extract) works
      correctly with automake-generated Makefiles
    - Add LAPACK_VERSION=current support via git clone/checkout
    - Unify all patch application to -p1; guard patch-print and patch-mingw
      with existence test (-f) instead of unconditional application
    - Introduce LAPACK_VERSION_KEY to select PATCHES_x_y_z generically
    - Convert 3.9.1 patch files from -p0 to -p1 format
    - Remove executable bit from all patch files under patches/
    - Remove stray backup file patch-ddrvst.f~

--- external/lapack/patches/3.9.1/patch-ztsqr01.f
+++ external/lapack/patches/3.9.1/patch-ztsqr01.f
@@ -1,5 +1,5 @@
---- TESTING/LIN/ztsqr01.f~	2021-03-26 03:25:15.000000000 +0900
-+++ TESTING/LIN/ztsqr01.f	2026-01-28 07:37:57.411322337 +0900
+--- a/TESTING/LIN/ztsqr01.f~	2021-03-26 03:25:15.000000000 +0900
++++ b/TESTING/LIN/ztsqr01.f	2026-01-28 07:37:57.411322337 +0900
 @@ -96,8 +96,9 @@
  *     ..
  *     .. Local allocatable arrays
