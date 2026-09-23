c827b786c674eec4df3587ce61d5676f63b4e7a2
Nakata Maho <maho.nakata@gmail.com>
2026-09-23 09:41:06 +0900

    Skip directories in macOS dylib fixup

--- misc/fix_dylib_macOS.sh
+++ misc/fix_dylib_macOS.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
 echo "fix dylibs for dir: $1, prefix $2"
-DIR=$1
-PREFIX=$2
-cd $DIR
-FILES=`ls`
-for _file in $FILES; do
-    echo $_file
-    otool -L $_file | grep "external/mpc/../i/MPC/lib/libmpc"
+DIR=${1:-}
+PREFIX=${2:-}
+if [ ! -d "$DIR" ]; then
+    echo "skip missing directory: $DIR"
+    exit 0
+fi
+cd "$DIR" || exit 1
+for _file in *; do
+    [ -f "$_file" ] || continue
+    echo "$_file"
+    otool -L "$_file" | grep "external/mpc/../i/MPC/lib/libmpc"
     if [ $? = 0 ]; then
-        ORG=`otool -L $_file  | grep MPC | awk '{print $1}'` ;\
-        _FIX=`otool -L $_file | grep MPC | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
+        ORG=`otool -L "$_file"  | grep MPC | awk '{print $1}'` ;\
+        _FIX=`otool -L "$_file" | grep MPC | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX="$PREFIX/lib/$_FIX" ;\
         echo "install_name_tool -change $ORG $FIX $_file" ;\
-        install_name_tool -change $ORG $FIX $_file
+        install_name_tool -change "$ORG" "$FIX" "$_file"
     fi
-    otool -L $_file | grep "external/mpfr/../i/MPFR/lib/libmpfr"
+    otool -L "$_file" | grep "external/mpfr/../i/MPFR/lib/libmpfr"
     if [ $? = 0 ]; then
-        ORG=`otool -L $_file  | grep MPFR | awk '{print $1}'` ;\
-        _FIX=`otool -L $_file | grep MPFR | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
+        ORG=`otool -L "$_file"  | grep MPFR | awk '{print $1}'` ;\
+        _FIX=`otool -L "$_file" | grep MPFR | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX="$PREFIX/lib/$_FIX" ;\
         echo "install_name_tool -change $ORG $FIX $_file" ;\
-        install_name_tool -change $ORG $FIX $_file
+        install_name_tool -change "$ORG" "$FIX" "$_file"
     fi
-    otool -L $_file | grep "external/gmp/../i/GMP/lib/libgmpxx"
+    otool -L "$_file" | grep "external/gmp/../i/GMP/lib/libgmpxx"
     if [ $? = 0 ]; then
-        ORG=`otool -L $_file  | grep GMP | grep gmpxx | awk '{print $1}'` ;\
-        _FIX=`otool -L $_file | grep GMP | grep gmpxx | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
+        ORG=`otool -L "$_file"  | grep GMP | grep gmpxx | awk '{print $1}'` ;\
+        _FIX=`otool -L "$_file" | grep GMP | grep gmpxx | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX="$PREFIX/lib/$_FIX" ;\
         echo "install_name_tool -change $ORG $FIX $_file" ;\
-        install_name_tool -change $ORG $FIX $_file
+        install_name_tool -change "$ORG" "$FIX" "$_file"
     fi
-    otool -L $_file | grep "external/gmp/../i/GMP/lib/libgmp"
+    otool -L "$_file" | grep "external/gmp/../i/GMP/lib/libgmp"
     if [ $? = 0 ]; then
-        ORG=`otool -L $_file  | grep GMP | grep -v gmpxx | awk '{print $1}'` ;\
-        _FIX=`otool -L $_file | grep GMP | grep -v gmpxx | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
+        ORG=`otool -L "$_file"  | grep GMP | grep -v gmpxx | awk '{print $1}'` ;\
+        _FIX=`otool -L "$_file" | grep GMP | grep -v gmpxx | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX="$PREFIX/lib/$_FIX" ;\
         echo "install_name_tool -change $ORG $FIX $_file" ;\
-        install_name_tool -change $ORG $FIX $_file
+        install_name_tool -change "$ORG" "$FIX" "$_file"
     fi
-    otool -L $_file | grep "external/qd/../i/QD/lib/libqd"
+    otool -L "$_file" | grep "external/qd/../i/QD/lib/libqd"
     if [ $? = 0 ]; then
-        ORG=`otool -L $_file  | grep QD | awk '{print $1}'` ;\
-        _FIX=`otool -L $_file | grep QD | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX=`echo $PREFIX/lib/$_FIX` ;\
+        ORG=`otool -L "$_file"  | grep QD | awk '{print $1}'` ;\
+        _FIX=`otool -L "$_file" | grep QD | awk '{print $1}' | sed 's|/| |g' | awk '{print $NF}'` ; FIX="$PREFIX/lib/$_FIX" ;\
         echo "install_name_tool -change $ORG $FIX $_file" ;\
-        install_name_tool -change $ORG $FIX $_file
+        install_name_tool -change "$ORG" "$FIX" "$_file"
     fi
 done
